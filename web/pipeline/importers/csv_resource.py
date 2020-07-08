5c56de1f4956a6d4a886dd679c2517759a96f7af
Shirley Yin <shirley@countable.ca>
2020-07-08 10:13:23 -0700

    move code into functions and modules

---
+++ web/pipeline/importers/csv_resource.py
@@ -0,0 +1,45 @@
+import csv
+import os
+
+from django.conf import settings
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.measure import D
+
+
+from pipeline.importers.communities import import_communities_from_csv
+
+COMMUNITIES_CSV = "COMMUNITIES_V2.csv"
+FILES_DIR = settings.BASE_DIR
+
+
+def import_csv_resource(resource_type):
+    print("resource_type", resource_type)
+    pass
+
+    communities_file_path = os.path.join(FILES_DIR, COMMUNITIES_CSV)
+    import_communities_from_csv(communities_file_path)
+
+
+
+# TODO: this is unused right now
+# def get_route_planner_distance(origin, destination):
+#     """
+#     Use the BC Route Planner to get driving distances between points.
+#     """
+#     api_url = "https://router.api.gov.bc.ca/distance.json?points={origin_lng}%2C{origin_lat}%2C{destination_lng}%2C{destination_lat}".format(
+#         origin_lng=origin.longitude(),
+#         origin_lat=origin.latitude(),
+#         destination_lng=destination.longitude(),
+#         destination_lat=destination.latitude())
+
+#     response = requests.get(
+#         api_url,
+#         headers={"accept": "*/*", "apikey": settings.ROUTE_PLANNER_API_KEY})
+
+#     route = response.json()
+#     distance = route["distance"]
+#     travel_time = route["time"]
+#     travel_time_display = route["timeText"]
+
+#     return distance, travel_time, travel_time_display
---
+++ web/pipeline/importers/databc_resource.py
@@ -0,0 +1,65 @@
+import requests
+
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from django.core.exceptions import FieldDoesNotExist
+
+from pipeline.constants import DATABC_RESOURCES
+from pipeline.models import (
+    Community,
+)
+
+API_URL = "https://catalogue.data.gov.bc.ca/api/3/action/datastore_search?resource_id={resource_id}&limit=10000"
+
+
+def import_databc_resource(resource_type):
+    if resource_type not in ['all', *DATABC_RESOURCES.keys()]:
+        print("Error: Resource type {} not supported".format(resource_type))
+        return
+
+    resource = DATABC_RESOURCES[resource_type]
+    resource_id = resource['resource_id']
+    response = requests.get(API_URL.format(resource_id=resource_id))
+
+    if response.status_code != 200:
+        print("Failed to download dataset {} {}".format(resource_type, resource_id))
+        print("Error: {} {}".format(response.status_code, response.content))
+        return
+
+    data = response.json()["result"]["records"]
+    Model = resource['model']
+
+    for row in data:
+        try:
+            point = Point(
+                float(row[Model.LONGITUDE_FIELD]),
+                float(row[Model.LATITUDE_FIELD]),
+                srid=3005
+            )
+        except TypeError:
+            print("Skipping error:", row[Model.NAME_FIELD], "has no geometry!")
+            continue
+        closest_community = Community.objects.annotate(
+            distance=Distance('point', point)
+        ).order_by('distance').first()
+        # containing_subdiv = CensusSubdivision.objects.get(geom__contains=point)
+        instance = Model.objects.get_or_create(
+            name=row[Model.NAME_FIELD],
+            point=point,
+            location_type=resource_type,
+            community=closest_community
+        )[0]
+
+        for field_name, field_value in row.items():
+            # loop over fields, and if the field exists
+            # on the model, import this field
+            print("field_name, field_value", field_name, field_value)
+            if isinstance(field_value, str):
+                try:
+                    field_value = field_value[:Model._meta.get_field(field_name.lower()).max_length]
+                except FieldDoesNotExist:
+                    pass
+            setattr(instance, field_name.lower(), field_value)
+
+        print(instance)
+        instance.save()
