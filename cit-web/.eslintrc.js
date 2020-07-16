6bea29c85a826d274a4eac603d6e6215bee34957
Aaron Ahn <aaron@countable.ca>
2020-07-15 20:41:51 -0700

    prototype finish

--- cit-web/.eslintrc.js
+++ cit-web/.eslintrc.js
@@ -15,7 +15,9 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-unused-vars": "off"
   },
   overrides: [
     {
