9ae206e89d816be9e5ca9e4e44141e552dcd4492
Shirley Yin <shirley@countable.ca>
2020-07-24 17:09:07 -0700

    add calculating cached fields to bootstrap script

--- web/pipeline/management/commands/bootstrap.py
+++ web/pipeline/management/commands/bootstrap.py
@@ -3,6 +3,9 @@ from django.core.management.base import BaseCommand
 from pipeline.importers.csv_resource import import_csv_resources
 from pipeline.importers.databc_resource import import_databc_resources
 from pipeline.importers.shp_resource import import_shp_resources
+from pipeline.importers.utils import (
+    calculate_community_num_schools, calculate_community_num_hospitals, calculate_community_num_courts,
+    calculate_community_num_timber_facilities)
 
 
 class Command(BaseCommand):
@@ -16,3 +19,9 @@ class Command(BaseCommand):
         # Note: Community imports first because it is first in the list of resources in constants.py
         import_csv_resources("all")
         import_databc_resources("all")
+
+        # calculate cached fields
+        calculate_community_num_schools()
+        calculate_community_num_hospitals()
+        calculate_community_num_courts()
+        calculate_community_num_timber_facilities()
