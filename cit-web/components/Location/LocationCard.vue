b5ec742f6f94bc349d36859ac56c8faa2a6ae849
Aaron Ahn <aaron@countable.ca>
2020-08-24 12:28:41 -0700

    link map and modal

--- cit-web/components/Location/LocationCard.vue
+++ cit-web/components/Location/LocationCard.vue
@@ -8,7 +8,7 @@
         </div>
       </v-card-text>
       <v-card-actions class="pt-0">
-        <v-btn x-small text color="primary accent-4">
+        <v-btn x-small text color="primary accent-4" @click="triggerFind">
           Find On Map
           <v-icon small dark>mdi-map-marker</v-icon>
         </v-btn>
@@ -61,6 +61,10 @@ export default class LocationCard extends Vue {
         icon: 'mdi-hospital-box',
       },
     }[location]
+  }
+
+  triggerFind(e) {
+    this.$emit('map-find', [this.location.longitude, this.location.latitude])
   }
 }
 </script>
