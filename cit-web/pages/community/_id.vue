454711f65336da481e87af7d4e6b75b696ff1635
Aaron Ahn <aaron@countable.ca>
2020-08-25 15:11:56 -0700

    center control and add legend back

--- cit-web/pages/community/_id.vue
+++ cit-web/pages/community/_id.vue
@@ -316,6 +316,46 @@ V<template>
         <v-icon small>mdi-bullseye</v-icon>
       </v-btn>
     </div>
+
+    <div ref="legendControl">
+      <v-card max-width="190">
+        <v-list>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>Internet Speeds</v-list-item-title>
+              <v-list-item-subtitle
+                >50/10
+                <div
+                  class="legend-icon"
+                  style="background-color: #8572d3;"
+                ></div
+              ></v-list-item-subtitle>
+              <v-list-item-subtitle
+                >25/5
+                <div
+                  class="legend-icon"
+                  style="background-color: #ec67ad;"
+                ></div
+              ></v-list-item-subtitle>
+              <v-list-item-subtitle
+                >10/2
+                <div
+                  class="legend-icon"
+                  style="background-color: #ff826f;"
+                ></div
+              ></v-list-item-subtitle>
+              <v-list-item-subtitle
+                >5/1
+                <div
+                  class="legend-icon"
+                  style="background-color: #f7ba44;"
+                ></div
+              ></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -451,8 +491,8 @@ export default class CommunityDetail extends Vue {
     return true
   }
 
-  getCenterControl() {
-    return class CenterControl {
+  getControlFactory() {
+    return class getControlFactory {
       el = null
       constructor(el) {
         this.el = el
@@ -461,7 +501,7 @@ export default class CommunityDetail extends Vue {
       onAdd(map) {
         this.map = map
         this.container = document.createElement('div')
-        this.container.className = 'mapboxgl-ctrl center-control-ctrl'
+        this.container.className = 'mapboxgl-ctrl'
         this.container.appendChild(this.el)
         return this.container
       }
@@ -640,9 +680,12 @@ export default class CommunityDetail extends Vue {
       ])
       .addTo(map)
 
-    const CenterControl = this.getCenterControl()
-    const centerControl = new CenterControl(this.$refs.centerControl)
-    map.addControl(centerControl, 'bottom-right')
+    const MapBoxControl = this.getControlFactory()
+    const centerControl = new MapBoxControl(this.$refs.centerControl)
+    map.addControl(centerControl, 'top-right')
+
+    const legendControl = new MapBoxControl(this.$refs.legendControl)
+    map.addControl(legendControl, 'bottom-right')
 
     map.on('click', function (e) {
       console.log(e)
@@ -700,16 +743,6 @@ export default class CommunityDetail extends Vue {
   width: 35px;
   margin-right: 50%;
   margin-top: 10px;
-}
-
-.legend {
-  bottom: 30px;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-  font: 12px/20px;
-  position: absolute;
-  right: 10px;
-  z-index: 1;
-  width: 180px;
 }
 
 .community-details-sidebar {
