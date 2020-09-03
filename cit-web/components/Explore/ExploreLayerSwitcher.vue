3215da72fb636564572fd3b06163316d1b81b186
Aaron Ahn <aaron@countable.ca>
2020-09-03 12:19:55 -0700

    layer switcher component, and update icons for commmunity detail page

---
+++ cit-web/components/Explore/ExploreLayerSwitcher.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Explore Layer Switcher
+    <LayerSwitcher></LayerSwitcher>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+import LayerSwitcher from '~/components/LayerSwitcher'
+@Component({
+  LayerSwitcher,
+})
+export default class ExploreLayerSwitcher extends Vue {}
+</script>
---
+++ cit-web/components/LayerSwitcher.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-menu
+    v-model="menu"
+    class="pa-0 ma-0 rounded"
+    bottom
+    transition="scale-transition"
+    :close-on-content-click="false"
+    offset-y
+    nudge-bottom="5"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn v-bind="attrs" filter v-on="on">
+        Layers
+      </v-btn>
+    </template>
+    <v-card :width="cardWidth">
+      <v-list dark>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Layers</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-card-text>
+        Switches should go here
+      </v-card-text>
+    </v-card>
+  </v-menu>
+</template>
+
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class LayerSwitcher extends Vue {
+  menu = false
+}
+</script>
