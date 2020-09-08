3d0feea6a6a7d1086fd8888d0ff06a36f578ccee
Aaron Ahn <aaron@countable.ca>
2020-09-07 19:50:32 -0700

    updates

--- cit-web/components/Compare.vue
+++ cit-web/components/Compare.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <CompareAutocomplete
-      :items="communities"
-      item-value="id"
-      item-text="place_name"
-      :multiple="true"
-      @change="handleChange"
-    ></CompareAutocomplete>
+    <v-container fluid>
+      <v-row>
+        <v-col :cols="selectCols">
+          <CompareSelect @changed="handleSelectChange"></CompareSelect>
+        </v-col>
+        <v-col v-if="showAutoComplete" cols="9">
+          <CompareAutocomplete
+            :items="items"
+            item-value="id"
+            :item-text="itemText"
+            :multiple="true"
+            @change="handleChange"
+          ></CompareAutocomplete>
+        </v-col>
+      </v-row>
+    </v-container>
 
     <div v-if="loading">
       <h6 class="text-h6 text-center mt-10 mb-10">Generating your report</h6>
@@ -33,24 +42,54 @@
 <script>
 import { Component, Vue, namespace } from 'nuxt-property-decorator'
 import CompareAutocomplete from '~/components/CompareAutocomplete'
+import CompareSelect from '~/components/CompareSelect'
 import Report from '~/components/CommunityDetails/Report'
 const commModule = namespace('communities')
 @Component({
   CompareAutocomplete,
   Report,
+  CompareSelect,
 })
 export default class Compare extends Vue {
   @commModule.Getter('getCommunities') communities
   @commModule.Getter('getRegionalDistricts') regionalDistricts
-  autocomplete = []
   loading = true
-  /*
-  get cids() {
+
+  get itemText() {
+    if (this.mode === 'Communities') {
+      return 'place_name'
+    } else if (this.mode === 'Regional Districts') {
+      return 'name'
+    } else {
+      return ''
+    }
+  }
+
+  get items() {
+    if (this.mode === 'Communities') {
+      return this.communities
+    } else if (this.mode === 'Regional Districts') {
+      return this.regionalDistricts
+    } else {
+      return []
+    }
+  }
+
+  get allCids() {
     return this.communities.map((c) => c.id.toString())
   }
-  */
+
+  get selectCols() {
+    return this.mode === 'All Of BC' ? 12 : 3
+  }
+
+  get showAutoComplete() {
+    return this.mode !== 'All Of BC'
+  }
 
   cids = []
+
+  mode = 'All Of BC'
 
   remove(item) {
     const index = this.autocomplete.indexOf(item.id)
@@ -58,8 +97,28 @@ export default class Compare extends Vue {
   }
 
   handleChange(data) {
-    const cids = data.map((cid) => cid.toString())
+    console.log(data)
+    let cids = []
+    if (this.mode === 'Communities') {
+      cids = data.map((cid) => cid.toString())
+    } else if (this.mode === 'Regional Districts') {
+      const temp = []
+      data.map((rid) => {
+        temp.push(this.communities.filter((c) => c.regional_district !== rid))
+      })
+      console.log('Temp', temp)
+      cids = temp.map((cid) => cid.toString())
+    }
     this.cids = cids
+  }
+
+  handleSelectChange(data) {
+    this.mode = data
+    if (data === 'All Of BC') {
+      this.cids = this.allCids
+    } else {
+      this.cids = []
+    }
   }
 }
 </script>
---
+++ cit-web/components/CompareSelect.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-select
+      v-model="selected"
+      :items="items"
+      label="Solo field"
+      solo
+      @change="$emit('changed', selected)"
+    ></v-select>
+  </div>
+</template>
+
+<script>
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+@Component
+export default class CompareSelect extends Vue {
+  @Prop({
+    default: () => ['All Of BC', 'Regional Districts', 'Communities'],
+    type: Array,
+  })
+  items
+
+  selected = null
+}
+</script>
