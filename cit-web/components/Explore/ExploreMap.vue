6d94010d439b0654548c5cee147764996ecb3c1b
Aaron Ahn <aaron@countable.ca>
2020-09-15 14:08:51 -0700

    clustering

--- cit-web/components/Explore/ExploreMap.vue
+++ cit-web/components/Explore/ExploreMap.vue
@@ -92,6 +92,9 @@ export default class Explore extends Vue {
     this.map.addSource('communities', {
       type: 'geojson',
       data: this.communityGeoJSON,
+      cluster: true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50,
     })
   }
 
@@ -112,7 +115,42 @@ export default class Explore extends Vue {
       },
       paint: { 'text-halo-width': 1, 'text-halo-blur': 1 },
     })
-    console.log('adding layers')
+
+    this.map.addLayer({
+      id: 'clusters',
+      type: 'circle',
+      source: 'communities',
+      filter: ['has', 'point_count'],
+      paint: {
+        // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+        // with three steps to implement three types of circles:
+        //   * Blue, 20px circles when point count is less than 100
+        //   * Yellow, 30px circles when point count is between 100 and 750
+        //   * Pink, 40px circles when point count is greater than or equal to 750
+        'circle-color': [
+          'step',
+          ['get', 'point_count'],
+          '#51bbd6',
+          100,
+          '#f1f075',
+          750,
+          '#f28cb1',
+        ],
+        'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
+      },
+    })
+
+    this.map.addLayer({
+      id: 'cluster-count',
+      type: 'symbol',
+      source: 'communities',
+      filter: ['has', 'point_count'],
+      layout: {
+        'text-field': '{point_count_abbreviated}',
+        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+        'text-size': 12,
+      },
+    })
   }
 
   resize() {
