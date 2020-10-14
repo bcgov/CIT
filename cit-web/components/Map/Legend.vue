372cf6f4f6de7caa1264f324f308bff95001f15a
Aaron Ahn <aaron@countable.ca>
2020-10-14 14:41:49 -0700

    some legends drawn

--- cit-web/components/Map/Legend.vue
+++ cit-web/components/Map/Legend.vue
@@ -2,6 +2,8 @@
   <div>
     <InternetSpeed></InternetSpeed>
     <Municipal></Municipal>
+    <WildfireLegend></WildfireLegend>
+    <CensusLegend></CensusLegend>
   </div>
 </template>
 
@@ -9,9 +11,14 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 import InternetSpeed from '~/components/Map/Legends/InternetSpeed'
 import Municipal from '~/components/Map/Legends/Municipal'
+import WildfireLegend from '~/components/Map/Legends/WildfireLegend'
+import CensusLegend from '~/components/Map/Legends/CensusLegend'
+
 @Component({
   InternetSpeed,
   Municipal,
+  WildfireLegend,
+  CensusLegend,
 })
 export default class Legend extends Vue {}
 </script>
