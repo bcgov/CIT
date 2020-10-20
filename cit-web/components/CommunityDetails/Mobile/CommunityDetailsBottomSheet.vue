53b1a9a65d359a3c51a2492fc76bb4a272d79a14
Aaron Ahn <aaron@countable.ca>
2020-10-20 14:57:32 -0700

    mobile design change

---
+++ cit-web/components/CommunityDetails/Mobile/CommunityDetailsBottomSheet.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-bottom-sheet :value="sheetOpen">
+      <v-sheet height="90vh">
+        Sheet Test
+        <slot></slot>
+      </v-sheet>
+    </v-bottom-sheet>
+  </div>
+</template>
+
+<script>
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class CommunityDetailsBottomSheet extends Vue {
+  @Prop({ default: false, type: Boolean }) sheetOpen
+}
+</script>
---
+++ cit-web/components/CommunityDetails/Mobile/CommunityDetailsFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-app-bar
+      fixed
+      bottom
+      height="40"
+      color="primary"
+      class="elevation-5"
+      @click="$emit('openSheet', true)"
+    >
+      <v-toolbar-title class="text-body-1 white--text"
+        >Vancouver</v-toolbar-title
+      >
+      <v-spacer> </v-spacer>
+      <v-btn icon fab>
+        <v-icon color="white">mdi-chevron-up</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class CommuntityDetailsFooter extends Vue {}
+</script>
