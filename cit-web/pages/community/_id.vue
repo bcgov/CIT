78bbb386323b5bfc54aeb2140b06af12edca464c
Aaron Ahn <aaron@countable.ca>
2020-10-23 15:55:44 -0700

    radio for switching

--- cit-web/pages/community/_id.vue
+++ cit-web/pages/community/_id.vue
@@ -88,6 +88,29 @@
                         @viewReports="viewReports"
                       >
                         <div class="pl-4 pr-4">
+                          <div class="mt-5">
+                            <h6
+                              class="pa-0 ma-0 text-subtitle-1 font-weight-bold my-5"
+                              style="line-height: 0;"
+                            >
+                              Show facilities by
+                            </h6>
+                            <div>
+                              <v-radio-group
+                                v-model="assetMode"
+                                class="pa-0 ma-0"
+                              >
+                                <v-radio
+                                  value="driving"
+                                  label="Driving Distance"
+                                ></v-radio>
+                                <v-radio
+                                  value="boundary"
+                                  label="Municial Boundary"
+                                ></v-radio>
+                              </v-radio-group>
+                            </div>
+                          </div>
                           <p class="text-center pa-0 ma-0 text-body-1 mt-3">
                             {{ assetModeText }}
                             <a
@@ -101,19 +124,6 @@
                             v-if="assetMode === 'driving'"
                             @mouseup="handleEnd"
                           ></AssetSlider>
-                          <div class="text-center">
-                            <v-btn
-                              small
-                              color="primary"
-                              class="text-body-1 text-capitalize mt-2"
-                              @click="handleAssetModeChange"
-                            >
-                              <v-icon small class="mr-2">{{
-                                assetModeButtonIcon
-                              }}</v-icon>
-                              {{ assetModeButtonText }}</v-btn
-                            >
-                          </div>
                         </div>
                       </Sidebar>
                       <CommunityDetailsMap
