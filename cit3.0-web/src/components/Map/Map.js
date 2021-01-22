1ac01c4c216f48bff5ea01e38c24f47c9839cfbf
HypnoMama <sarahhermsen@gmail.com>
2021-01-22 15:13:27 -0800

    modify zoom level

--- cit3.0-web/src/components/Map/Map.js
+++ cit3.0-web/src/components/Map/Map.js
@@ -12,11 +12,11 @@ export default function Map(props) {
     return (
         <MapContainer 
           center={props.coords} 
-          zoom={13} 
+          zoom={16} 
           scrollWheelZoom={false}
           style={{ width: '100%', height:'100%'}}
         >
-          <ChangeView center={props.coords} zoom={13} />
+          <ChangeView center={props.coords} zoom={16} />
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
