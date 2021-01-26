668b37cd8a16a58017b5abac9a3873e63486c603
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-01-26 12:13:49 -0800

    Updated length to 255

--- web/pipeline/models/investment.py
+++ web/pipeline/models/investment.py
@@ -1,5 +1,5 @@
 from django.contrib.gis.db import models
 
 class Investment(models.Model):
-    address = models.CharField(max_length=127, null=True)
+    address = models.CharField(max_length=255, null=True)
     point = models.PointField(srid=4326, null=True, blank=False)
