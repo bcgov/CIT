cc0e90fc90cd7ff2203efad08bc2e964e2b40665
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-01-28 11:05:30 -0800

    Updated API and database schema for investments

--- web/pipeline/models/investment.py
+++ web/pipeline/models/investment.py
@@ -1,5 +1,5 @@
 from django.contrib.gis.db import models
 
 class Investment(models.Model):
-    address = models.CharField(max_length=255, null=True)
-    point = models.PointField(srid=4326, null=True, blank=False)
+    address = models.CharField(max_length=255, null=False)
+    point = models.PointField(srid=4326, null=False, blank=False)
