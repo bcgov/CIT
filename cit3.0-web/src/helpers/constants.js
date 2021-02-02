d66d95f8e35d73e421ac8389d2366fe986ffeca7
HypnoMama <sarahhermsen@gmail.com>
2021-02-01 16:44:35 -0800

    add constants and helper functions

---
+++ cit3.0-web/src/helpers/constants.js
@@ -0,0 +1,42 @@
+export const HOSPITALS = {
+  colourName: "green",
+  colourHex: "#2AAD27",
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+};
+export const SCHOOLS = {
+  colourName: "gold",
+  colourHex: "#FFD326",
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png",
+};
+export const POST_SECONDARY = {
+  colourName: "violet",
+  colourHex: "#9C2BCB",
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png",
+};
+export const COURTS = {
+  colourName: "grey",
+  colourHex: "#7B7B7B",
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png",
+};
+export const WALKIN_CLINICS = {
+  colourName: "red",
+  colourHex: "#CB2B3E",
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+};
+export const NATURAL_RESOURCE_PROJECTS = {
+  colourName: "yellow",
+  colourHex: "#CAC428",
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png",
+};
+export const ECONOMIC_PROJECTS = {
+  colourName: "orange",
+  colourHex: "#CB8427",
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png",
+};
