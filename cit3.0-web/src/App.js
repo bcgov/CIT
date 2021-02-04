738177b9c81d8610d62423553e56e07acffa2adc
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-02-04 09:34:39 -0800

    Added app.js

--- cit3.0-web/src/App.js
+++ cit3.0-web/src/App.js
@@ -1,27 +1,36 @@
 import "@bcgov/bootstrap-theme/dist/css/bootstrap-theme.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import { useHistory } from "react-router-dom";
 import { Header, Footer } from "shared-components";
 import MapContainer from "./components/MapContainer/MapContainer";
+import EDODashboard from "./components/Page/EDODashboard/EDODashboard";
 
 function App() {
   const header = {
     name: "Community Information Tool",
-    history: useHistory(),
+    history: {},
   };
 
   return (
     <div style={{ minHeight: "100vh" }}>
       <Header header={header} />
-
-      <div
-        style={{ height: "80vh" }}
-        className="container d-flex flex-column justify-content-center align-items-center"
-      >
-        <MapContainer />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <EDODashboard />
+          </Route>
+          <Route path="/addInvestment">
+            <div
+              style={{ height: "80vh" }}
+              className="container d-flex flex-column justify-content-center align-items-center"
+            >
+              <MapContainer />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
 
       <div className="footer">
         <Footer />
