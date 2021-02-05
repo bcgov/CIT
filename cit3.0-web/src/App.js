6bec9cc98790cb2bd863947f5d5379cf4e04bee9
HypnoMama <sarahhermsen@gmail.com>
2021-02-05 10:34:52 -0800

    route to addOpportunity page

--- cit3.0-web/src/App.js
+++ cit3.0-web/src/App.js
@@ -1,38 +1,28 @@
 import "@bcgov/bootstrap-theme/dist/css/bootstrap-theme.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Header, Footer } from "shared-components";
-import MapContainer from "./components/MapContainer/MapContainer";
+import AddOpportunity from "./components/Page/AddOpportunity/AddOpportunity";
 import EDODashboard from "./components/Page/EDODashboard/EDODashboard";
 
 function App() {
-  const [nearbyResources, setNearbyResources] = useState({});
   const header = {
     name: "Community Information Tool",
     history: {},
   };
 
   return (
-    <div style={{ minHeight: "100vh" }}>
+    <div className="app-container">
       <Header header={header} />
       <Router>
         <Switch>
           <Route exact path="/">
             <EDODashboard />
           </Route>
-          <Route path="/addInvestment">
-            <div
-              style={{ height: "80vh" }}
-              className="container d-flex flex-column justify-content-center align-items-center"
-            >
-              <MapContainer
-                nearbyResources={nearbyResources}
-                setNearbyResources={setNearbyResources}
-              />
-            </div>
+          <Route path="/addOpportunity">
+            <AddOpportunity />
           </Route>
         </Switch>
       </Router>
