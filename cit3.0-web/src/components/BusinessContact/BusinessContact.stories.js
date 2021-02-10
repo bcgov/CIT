6934865d3bab113bac8eaf91d9d8e6227136957f
Zac Pez <Zachary.Pez@nttdata.com>
2021-02-10 12:50:19 -0800

    CIT-62: Changed email to have mailto, fixed ke value style

--- cit3.0-web/src/components/BusinessContact/BusinessContact.stories.js
+++ cit3.0-web/src/components/BusinessContact/BusinessContact.stories.js
@@ -4,3 +4,4 @@ import { storiesOf } from '@storybook/react';
 import BusinessContact from './BusinessContact';
 
 storiesOf('BusinessContact', module).add('default', () => <BusinessContact />);
+storiesOf('BusinessContact', module).add('assigned', () => <BusinessContact name="Jane Doe" email="jane.doe@example.com" />);
