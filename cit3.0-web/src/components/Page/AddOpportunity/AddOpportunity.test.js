55f3ca7e4233780d8f53428ebc54dda741143ba1
HypnoMama <sarahhermsen@gmail.com>
2021-02-10 17:04:41 -0800

    fix tests for MapContainer, add tests for AddOpportunity

--- cit3.0-web/src/components/Page/AddOpportunity/AddOpportunity.test.js
+++ cit3.0-web/src/components/Page/AddOpportunity/AddOpportunity.test.js
@@ -5,8 +5,34 @@ afterEach(cleanup);
 
 describe("AddOpportunity", () => {
   it("renders the Continue Button", () => {
-    const { getByText } = render(<AddOpportunity />);
+    const { getByText } = render(<AddOpportunity match={{}} />);
     const continueButton = getByText("Continue");
     expect(continueButton).toBeInTheDocument();
+  });
+  it("renders the Cancel Button", () => {
+    const { getByText } = render(<AddOpportunity match={{}} />);
+    const cancel = getByText(/Cancel/i);
+    expect(cancel).toBeInTheDocument();
+  });
+  it("renders the Address Search bar", () => {
+    const { container } = render(<AddOpportunity match={{}} />);
+    const addressInput = container.querySelector("input");
+    expect(addressInput).toBeInTheDocument();
+  });
+
+  it("renders the Portal Header", () => {
+    const { getByText } = render(<AddOpportunity match={{}} />);
+    const headerText = getByText(/Portal/i);
+    expect(headerText.textContent).toBe("Opportunity Portal");
+  });
+  it("renders the navigation header", () => {
+    const { getByText } = render(<AddOpportunity match={{}} />);
+    const navItem = getByText("Additional Info");
+    expect(navItem).toBeInTheDocument();
+  });
+  it("renders the Page Title Header", () => {
+    const { getByText } = render(<AddOpportunity match={{}} />);
+    const addOp = getByText("Add an Opportunity");
+    expect(addOp).toBeInTheDocument();
   });
 });
