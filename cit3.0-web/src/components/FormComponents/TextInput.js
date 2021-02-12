ca7680bc63f0456fa953053d6623857737c5b71b
HypnoMama <sarahhermsen@gmail.com>
2021-02-12 13:40:21 -0800

    Text area input and styling for forms

---
+++ cit3.0-web/src/components/FormComponents/TextInput.js
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+import "./input.css";
+
+export default function TextInput({
+  heading,
+  notes,
+  placeholder,
+  rows,
+  disabled,
+}) {
+  return (
+    <div className="d-flex flex-column w-100">
+      <p className="mb-0">{heading}</p>
+      <p className="mb-0" style={{ opacity: "0.5" }}>
+        {notes}
+      </p>
+      <textarea
+        type="textarea"
+        className="bcgov-text-input mb-4"
+        rows={rows}
+        placeholder={placeholder}
+        disabled={disabled}
+      />
+    </div>
+  );
+}
+
+TextInput.defaultProps = {
+  placeholder: null,
+  rows: 3,
+  disabled: false,
+};
+
+TextInput.propTypes = {
+  heading: PropTypes.string.isRequired,
+  notes: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  rows: PropTypes.number,
+  disabled: PropTypes.bool,
+};
