a64ef1b863d1261aea81f20be47bd63e7b07021e
HypnoMama <sarahhermsen@gmail.com>
2021-02-12 15:29:07 -0800

    add sub Capacity component for radios

---
+++ cit3.0-web/src/components/FormComponents/MaxCapRow.js
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import { Row, Col } from "react-bootstrap";
+import TextInput from "./TextInput";
+
+export default function MaxCapRow({ units }) {
+  return (
+    <>
+      <Row id="water-max-cap-label">
+        Indicate the maximum capacity per hour in cubic metres, if known
+      </Row>
+      <Row className="align-items-center">
+        <Col md className="pl-0 w-25 justify-content-center">
+          <TextInput rows={1} />
+        </Col>
+        <Col xs className="justify-content-center pl-0">
+          {units ? (
+            <p>{units}</p>
+          ) : (
+            <p>
+              m<sup>3</sup>/hour
+            </p>
+          )}
+        </Col>
+        <Col xs />
+      </Row>
+    </>
+  );
+}
+
+MaxCapRow.defaultProps = {
+  units: null,
+};
+
+MaxCapRow.propTypes = {
+  units: PropTypes.string,
+};
