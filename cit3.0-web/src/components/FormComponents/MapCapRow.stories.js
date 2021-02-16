d3f4b4d4dca3d5663410152c3647722eb854e83d
HypnoMama <sarahhermsen@gmail.com>
2021-02-16 12:49:06 -0800

    add stories for form components

---
+++ cit3.0-web/src/components/FormComponents/MapCapRow.stories.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import MaxCapRow from "./MaxCapRow";
+
+export default {
+  title: "MaximumCapacityRow",
+  component: MaxCapRow,
+};
+
+export const MaximumCapacityRowWithDefaultUnits = () => (
+  <MaxCapRow handleChange={action("change handled")} name="maxCap" />
+);
+
+export const MaximumCapacityRowWithUnits = () => (
+  <MaxCapRow
+    units="MMBTU/hour"
+    handleChange={action("change handled")}
+    name="maxCap"
+    value=""
+  />
+);
