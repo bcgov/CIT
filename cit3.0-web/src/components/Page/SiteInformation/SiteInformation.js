4ac12e89be88771a33f5b1790cc51ed637e9f924
Zac Pez <Zachary.Pez@nttdata.com>
2021-02-22 11:19:30 -0800

    cit-107: Border adjustments from mockup change

--- cit3.0-web/src/components/Page/SiteInformation/SiteInformation.js
+++ cit3.0-web/src/components/Page/SiteInformation/SiteInformation.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 import { Alert } from "shared-components";
+import { MdError } from "react-icons/md";
+import { Container } from "react-bootstrap";
 import PortalHeader from "../../Headers/PortalHeader/PortalHeader";
 import NavigationHeader from "../../Headers/NavigationHeader/NavigationHeader";
 import OpportunityView from "../../OpportunityView/OpportunityView";
@@ -21,27 +23,15 @@ export default function SiteInformation({ location }) {
     <>
       <PortalHeader />
       <NavigationHeader currentStep={2} />
-      <Alert
-        className="container"
-        icon={
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 24 24"
-            size="32"
-            height="32"
-            width="32"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
-          </svg>
-        }
-        type="warning"
-        styling="bcgov-warning-background"
-        element="This shows you all the information we could collect from many open
+      <Container className="p-0 mt-3">
+        <Alert
+          icon={<MdError size={32} />}
+          type="warning"
+          styling="bcgov-warning-background"
+          element="This shows you all the information we could collect from many open
         source BC Gov. databases"
-      />
+        />
+      </Container>
       <OpportunityView data={location.state} />
       <ButtonRow
         showPrevious
