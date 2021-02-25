f53b6d4eb50339bb56c55504e86e7e3b4a2fd461
HypnoMama <sarahhermsen@gmail.com>
2021-02-25 14:58:31 -0800

    add show/hide map and containing div

--- cit3.0-web/src/components/OpportunityList/OpportunityList.js
+++ cit3.0-web/src/components/OpportunityList/OpportunityList.js
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-// import { Col } from "react-bootstrap";
 import OpportunityListItem from "../OpportunityListItem/OpportunityListItem";
 
 export default function OpportunityList({ opportunities }) {
--- cit3.0-web/src/components/OpportunityMap/OpportunityMapContainer.js
+++ cit3.0-web/src/components/OpportunityMap/OpportunityMapContainer.js
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { CgCloseR } from "react-icons/cg";
+import { Row, Col } from "react-bootstrap";
 import OpportunitiesMap from "./OpportunitiesMap";
 
-export default function OpportunityMapContainer() {
+export default function OpportunityMapContainer({ setMapVisible }) {
   const [opportunities, setOpportunities] = useState(null);
+  const [showMap, setShowMap] = useState(true);
 
   useEffect(() => {
     axios
@@ -18,6 +20,32 @@ export default function OpportunityMapContainer() {
         console.log(err);
         setOpportunities([]);
       });
-  }, [opportunities]);
-  return <OpportunitiesMap opportunities={opportunities} />;
+  }, []);
+
+  const styles = {
+    close: {
+      height: "30px",
+      width: "30px",
+      position: "absolute",
+      right: "0",
+      top: "0",
+      zIndex: 1000,
+      cursor: "pointer",
+    },
+  };
+
+  const toggleMap = () => {
+    setShowMap(!showMap);
+    setMapVisible(!showMap);
+  };
+  return (
+    <>
+      <CgCloseR style={styles.close} onClick={toggleMap} />
+      {showMap && <OpportunitiesMap opportunities={opportunities} />}
+    </>
+  );
 }
+
+OpportunityMapContainer.propTypes = {
+  setMapVisible: PropTypes.func.isRequired,
+};
