b15699ee10064653e682d815188e155946945d19
HypnoMama <sarahhermsen@gmail.com>
2021-02-25 14:32:42 -0800

    fix to only show when we have opportunities to display

--- cit3.0-web/src/components/OpportunitiesListContainer/OpportunitiesListContainer.js
+++ cit3.0-web/src/components/OpportunitiesListContainer/OpportunitiesListContainer.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import axios from "axios";
-import PropTypes from "prop-types";
 import OpportunityList from "../OpportunityList/OpportunityList";
 import Paginator from "../Paginator/Paginator";
 
-export default function OpportunityListContainer({ opps }) {
+export default function OpportunityListContainer() {
   const [opportunities, setOpportunities] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [count, setCount] = useState(null);
@@ -26,29 +25,27 @@ export default function OpportunityListContainer({ opps }) {
       });
   }, [currentPage]);
   return (
-    <Col>
-      <Row>
-        {opportunities && (
+    opportunities && (
+      <Col>
+        <Row>
           <h4>{opportunities.length} Properties match your search</h4>
-        )}
-        <OpportunityList opportunities={opportunities} />
-      </Row>
+          <OpportunityList opportunities={opportunities} />
+        </Row>
 
-      <Row className="d-flex flex-column align-items-center justify-content-center p-2">
-        <Paginator
-          count={count}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-          pageSize={4}
-        />
-        <p>
-          Showing {pageSize} of {count} properties
-        </p>
-      </Row>
-    </Col>
+        {count && (
+          <Row className="d-flex flex-column align-items-center justify-content-center p-2">
+            <Paginator
+              count={count}
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+              pageSize={4}
+            />
+            <p>
+              Showing {pageSize} of {count} properties
+            </p>
+          </Row>
+        )}
+      </Col>
+    )
   );
 }
-
-OpportunityListContainer.propTypes = {
-  opps: PropTypes.arrayOf(PropTypes.shape).isRequired,
-};
