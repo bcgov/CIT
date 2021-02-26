7bf1a40699a9c588898964f1a00255000a637d2d
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-02-25 16:43:49 -0800

    Added unit test, updated story name

--- cit3.0-web/src/components/Flyout/Flyout.test.js
+++ cit3.0-web/src/components/Flyout/Flyout.test.js
@@ -1,6 +1,25 @@
-// TODO: make this an actual test
-describe("FilterPanel", () => {
-  it("Shows and hides the contents when the corresponding buttons are clicked", () => {
-    expect(true).toBe(true);
+import { fireEvent, render } from "@testing-library/react";
+import Flyout from "./Flyout";
+
+describe("Flyout", () => {
+  it("Changes the flyout button's CSS class when clicked", () => {
+    const { container, getByText } = render(
+      <Flyout>
+        <div>
+          <p>Test child paragraph</p>
+        </div>
+      </Flyout>
+    );
+
+    expect(
+      container.getElementsByClassName("icon").item(0)
+    ).toBeInTheDocument();
+    expect(
+      container.getElementsByClassName("icon").item(0).classList
+    ).toContain("closed");
+    fireEvent.click(getByText(/>>/i));
+    expect(
+      container.getElementsByClassName("icon").item(0).classList
+    ).toContain("open");
   });
 });
