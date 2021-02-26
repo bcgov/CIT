41ede5090d32647db4e4dcbe0d2601d285e79c55
HypnoMama <sarahhermsen@gmail.com>
2021-02-26 12:48:34 -0800

    add props to send down total count from map api call, remove columns, add margin to top

--- cit3.0-web/src/components/OpportunitiesListContainer/OpportunitiesListContainer.js
+++ cit3.0-web/src/components/OpportunitiesListContainer/OpportunitiesListContainer.js
@@ -1,51 +1,57 @@
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import { Row, Col, Container } from "react-bootstrap";
 import axios from "axios";
 import OpportunityList from "../OpportunityList/OpportunityList";
 import Paginator from "../Paginator/Paginator";
 
-export default function OpportunityListContainer() {
+export default function OpportunityListContainer({ totalCount }) {
   const [opportunities, setOpportunities] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const [count, setCount] = useState(null);
   const [pageSize, setPageSize] = useState(4);
   useEffect(() => {
     axios
-      .get(
-        `/api/pipeline/opportunities?page=${currentPage}&page_size=${pageSize}`
-      )
+      .get(`/api/opportunity/list?page=${currentPage}&page_size=${pageSize}`)
       .then((data) => {
-        console.log(data.data.results);
+        console.log("oppListCont: ", data.data.results);
         setOpportunities(data.data.results);
-        setCount(data.data.count);
+        // setCount(data.data.count);
       })
       .catch((err) => {
         console.log(err);
-        setOpportunities([]);
+        setOpportunities(null);
       });
   }, [currentPage]);
   return (
     opportunities && (
-      <Col>
-        <Row>
-          <h4>{opportunities.length} Properties match your search</h4>
+      <>
+        <Row className="mt-3">
+          {totalCount && <h4>{totalCount} Properties match your search</h4>}
           <OpportunityList opportunities={opportunities} />
         </Row>
 
-        {count && (
+        {totalCount && (
           <Row className="d-flex flex-column align-items-center justify-content-center p-2">
             <Paginator
-              count={count}
+              count={totalCount}
               setCurrentPage={setCurrentPage}
               currentPage={currentPage}
               pageSize={4}
             />
             <p>
-              Showing {pageSize} of {count} properties
+              Showing {pageSize} of {totalCount} properties
             </p>
           </Row>
         )}
-      </Col>
+      </>
     )
   );
 }
+
+OpportunityListContainer.defaultProps = {
+  totalCount: 0,
+};
+
+OpportunityListContainer.propTypes = {
+  totalCount: PropTypes.number,
+};
