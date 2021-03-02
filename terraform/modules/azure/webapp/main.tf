cca30818ae02867064b8d3f130426b621f9be220
Paul Dittaro <pdittaro@gmail.com>
2021-03-02 08:37:10 -0800

    Remove suffix on secret

--- terraform/modules/azure/webapp/main.tf
+++ terraform/modules/azure/webapp/main.tf
@@ -208,18 +208,18 @@ data "azurerm_subscription" "primary" {}
 
 resource "github_actions_secret" "registry_username" {
   repository       = var.github_repository
-  secret_name      = "REGISTRY_USERNAME2"
+  secret_name      = "REGISTRY_USERNAME"
   plaintext_value  = azurerm_container_registry.acr.admin_username
 }
 
 resource "github_actions_secret" "registry_password" {
   repository       = var.github_repository
-  secret_name      = "REGISTRY_PASSWORD2"
+  secret_name      = "REGISTRY_PASSWORD"
   plaintext_value  = azurerm_container_registry.acr.admin_password
 }
 
 resource "github_actions_secret" "registry_login_server" {
   repository       = var.github_repository
-  secret_name      = "REGISTRY_LOGIN_SERVER2"
+  secret_name      = "REGISTRY_LOGIN_SERVER"
   plaintext_value  = azurerm_container_registry.acr.login_server
 }
