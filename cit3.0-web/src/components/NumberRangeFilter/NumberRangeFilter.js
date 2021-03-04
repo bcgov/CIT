988c9ee5db576dd0028f8b4f138c6e698224379c
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-03-04 09:47:11 -0800

    Input range component is working wth validation

--- cit3.0-web/src/components/NumberRangeFilter/NumberRangeFilter.js
+++ cit3.0-web/src/components/NumberRangeFilter/NumberRangeFilter.js
@@ -2,22 +2,89 @@ import { useState } from "react";
 import "./NumberRangeFilter.css";
 import { Button } from "shared-components";
 import { Modal, Row, Container } from "react-bootstrap";
+import InputRange from "react-input-range";
 import TextInput from "../FormComponents/TextInput";
+import Validator from "../FormComponents/Validator";
+import "react-input-range/src/scss/index.scss";
 
 export default function NumberRangeFilter() {
   const [show, setShow] = useState(false);
+  const [inputRangeValue, setInputRangeValue] = useState({
+    min: 0,
+    max: 50000,
+  });
+  const [minInput, setMinInput] = useState(0);
+  const [maxInput, setMaxInput] = useState(50000);
+  const [validMax, setValidMax] = useState(true);
+  const [validMin, setValidMin] = useState(true);
 
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
-
-  const updateMax = () => console.log("Update Max");
-  const updateMin = () => console.log("Update Min");
 
   const maxValue = 50000;
   const minValue = 0;
 
   const minName = "number-range-min";
   const maxName = "number-range-max";
+
+  const validate = (name, value) => {
+    if (Number.isNaN(value)) {
+      return false;
+    }
+
+    if (value < minValue || value > maxValue) {
+      return false;
+    }
+
+    if (name === maxName && value < inputRangeValue.min) {
+      return false;
+    }
+
+    if (name === minName && value > inputRangeValue.max) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const updateMax = (name, newMax) => {
+    setMaxInput(newMax);
+    if (validate(name, Number(newMax))) {
+      setValidMax(true);
+      setInputRangeValue({ min: inputRangeValue.min, max: Number(newMax) });
+    } else {
+      setValidMax(false);
+    }
+  };
+
+  const updateMin = (name, newMin) => {
+    setMinInput(newMin);
+
+    if (validate(name, Number(newMin))) {
+      setValidMin(true);
+      setInputRangeValue({ min: Number(newMin), max: inputRangeValue.max });
+    } else {
+      setValidMin(false);
+    }
+  };
+
+  const updateTextFields = (value) => {
+    const updatedValue = { ...value };
+
+    // Fix component allowing you select values beyond min/max range by clicking the edges of the input range
+    if (updatedValue.min < minValue) {
+      updatedValue.min = minValue;
+    }
+    if (updatedValue.max > maxValue) {
+      updatedValue.max = maxValue;
+    }
+
+    setInputRangeValue(updatedValue);
+    setMinInput(updatedValue.min);
+    setMaxInput(updatedValue.max);
+    setValidMin(true);
+    setValidMax(true);
+  };
 
   return (
     <>
@@ -32,32 +99,54 @@ export default function NumberRangeFilter() {
         </Modal.Header>
         <Modal.Body>
           <p>Size of Property (in square feet)</p>
-          <p>slidebar goes here</p>
+          <div className="input-range-section">
+            <InputRange
+              maxValue={maxValue}
+              minValue={minValue}
+              value={inputRangeValue}
+              formatLabel={() => {}}
+              onChange={(value) => updateTextFields(value)}
+            />
+          </div>
           <Container fluid>
             <Row className="d-flex justify-content-between">
-              <div className="modal-text-input">
-                <TextInput
-                  aria-labelledby="water-max-cap-label"
-                  handleChange={updateMax}
-                  name={maxName}
-                  rows={1}
-                  value={maxValue}
-                />
-              </div>
               <div className="modal-text-input">
                 <TextInput
                   aria-labelledby="water-max-cap-label"
                   handleChange={updateMin}
                   name={minName}
                   rows={1}
-                  value={minValue}
+                  value={String(minInput)}
+                />
+              </div>
+              <div className="modal-text-input">
+                <TextInput
+                  aria-labelledby="water-max-cap-label"
+                  handleChange={updateMax}
+                  name={maxName}
+                  rows={1}
+                  value={String(maxInput)}
                 />
               </div>
             </Row>
+            {(!validMax || !validMin) && (
+              <Row className="d-flex justify-content-between">
+                {!validMin ? (
+                  <Validator message="Invalid min number" />
+                ) : (
+                  <div />
+                )}
+                {!validMax ? (
+                  <Validator message="Invalid max number" />
+                ) : (
+                  <div />
+                )}
+              </Row>
+            )}
           </Container>
         </Modal.Body>
         <Modal.Footer>
-          <p>Footer!</p>
+          <p>Save</p>
         </Modal.Footer>
       </Modal>
     </>
