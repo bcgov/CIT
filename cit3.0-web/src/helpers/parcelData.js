b41e10536f939e90c792a011c07bdc4834fbfafc
HypnoMama <sarahhermsen@gmail.com>
2021-03-08 14:05:12 -0800

    return null for error

--- cit3.0-web/src/helpers/parcelData.js
+++ cit3.0-web/src/helpers/parcelData.js
@@ -11,6 +11,10 @@ export const getPID = async (siteId) =>
       console.log("PID: ", data.data.pids);
       // parse here
       return data.data.pids;
+    })
+    .catch((err) => {
+      console.log("Error retrieving PIDs: ", err);
+      return null;
     });
 
 // TODO -> make sure to parse data as it may contain comma separated list of PIDS,
@@ -18,5 +22,8 @@ export const getPID = async (siteId) =>
 // run multiple calls or build up query string
 export async function getParcelData(pid) {
   const url = `https://openmaps.gov.bc.ca/geo/pub/WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW/wfs?service=wfs&version=2.0.0&request=GetFeature&outputFormat=application%2Fjson&TypeNames=pub:WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW&srsName=EPSG%3A3005&cql_filter=PID='${pid}'`;
-  return axios.get(url);
+  return axios
+    .get(url)
+    .then((data) => data)
+    .catch(() => null);
 }
