6a7cf2a0b807d43d3ca5c148d0edeb1f76665275
Zac Pez <Zachary.Pez@nttdata.com>
2021-03-11 13:48:39 -0800

    CIT-127: Added service queries

--- cit-api/pipeline/views/opportunity.py
+++ cit-api/pipeline/views/opportunity.py
@@ -1,19 +1,23 @@
-from django.utils.decorators import method_decorator
 from rest_framework import generics
 from rest_framework import pagination
+from django.db.models import Q
+import operator
 
 from pipeline.models.opportunity import Opportunity
 from pipeline.models.general import RegionalDistrict
 from pipeline.serializers.opportunity import OpportunitySerializer
+
 
 class LargeResultsSetPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
+
 class OpportunitiesList(generics.ListAPIView):
     pagination_class = LargeResultsSetPagination
     serializer_class = OpportunitySerializer
+
     def get_queryset(self):
         queryset = Opportunity.objects.all()
         queryset = queryset.filter(deleted=False)
@@ -41,6 +45,41 @@ class OpportunitiesList(generics.ListAPIView):
             regional_district_models = RegionalDistrict.objects.get(id=regional_district)
             if(regional_district_models is not None):
                 queryset = queryset.filter(geo_position__intersects=regional_district_models.geom)
+
+        connected_unknown = self.request.query_params.get('connected_unknown', None)
+        queryset = self.service_queryset(queryset, connected_unknown, 'opportunity_road_connected')
+        queryset = self.service_queryset(queryset, connected_unknown, 'opportunity_water_connected')
+        queryset = self.service_queryset(queryset, connected_unknown, 'opportunity_sewer_connected')
+        queryset = self.service_queryset(queryset, connected_unknown, 'opportunity_electrical_connected')
+
+        return queryset
+
+    def service_queryset(self, queryset, connected_unknown, service_name):
+        service_connected = self.request.query_params.get(service_name, None)
+        if(service_name == 'opportunity_road_connected'):
+            if(service_connected is not None and connected_unknown is None):
+                queryset = queryset.filter(Q(opportunity_road_connected=service_connected) |
+                                           Q(opportunity_road_connected='U'))
+            elif(service_connected is not None):
+                queryset = queryset.filter(opportunity_road_connected=service_connected)
+        elif(service_name == 'opportunity_water_connected'):
+            if(service_connected is not None and connected_unknown is None):
+                queryset = queryset.filter(Q(opportunity_water_connected=service_connected) |
+                                           Q(opportunity_water_connected='U'))
+            elif(service_connected is not None):
+                queryset = queryset.filter(opportunity_water_connected=service_connected)
+        elif(service_name == 'opportunity_sewer_connected'):
+            if(service_connected is not None and connected_unknown is None):
+                queryset = queryset.filter(Q(opportunity_sewer_connected=service_connected) |
+                                           Q(opportunity_sewer_connected='U'))
+            elif(service_connected is not None):
+                queryset = queryset.filter(opportunity_sewer_connected=service_connected)
+        elif(service_name == 'opportunity_electrical_connected'):
+            if(service_connected is not None and connected_unknown is None):
+                queryset = queryset.filter(Q(opportunity_electrical_connected=service_connected) |
+                                           Q(opportunity_electrical_connected='U'))
+            elif(service_connected is not None):
+                queryset = queryset.filter(opportunity_electrical_connected=service_connected)
 
         return queryset
 
