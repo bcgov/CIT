d738eccdba10c1ee2138d01b7546a8d19698ab49
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-03-11 17:00:01 -0800

    Fixed tests

--- cit3.0-web/src/components/CommunityOrPopulationProximityFilter/CommunityOrPopulationProximityFilter.test.js
+++ cit3.0-web/src/components/CommunityOrPopulationProximityFilter/CommunityOrPopulationProximityFilter.test.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import CommunityOrPopulationProximityFilter from './CommunityOrPopulationProximityFilter';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import CommunityOrPopulationProximityFilter from "./CommunityOrPopulationProximityFilter";
 
-describe('<CommunityOrPopulationProximityFilter />', () => {
-  test('it should mount', () => {
+describe("<CommunityOrPopulationProximityFilter />", () => {
+  test("it should mount", () => {
     render(<CommunityOrPopulationProximityFilter />);
-    
-    const communityOrPopulationProximityFilter = screen.getByTestId('CommunityOrPopulationProximityFilter');
+
+    const communityOrPopulationProximityFilter = screen.getByText(
+      "Hello filter"
+    );
 
     expect(communityOrPopulationProximityFilter).toBeInTheDocument();
   });
--- cit3.0-web/src/components/InputRangeWithTextboxes/InputRangeWithTextboxes.test.js
+++ cit3.0-web/src/components/InputRangeWithTextboxes/InputRangeWithTextboxes.test.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import InputRangeWithTextboxes from './InputRangeWithTextboxes';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import InputRangeWithTextboxes from "./InputRangeWithTextboxes";
 
-describe('<InputRangeWithTextboxes />', () => {
-  test('it should mount', () => {
-    render(<InputRangeWithTextboxes />);
-    
-    const inputRangeWithTextboxes = screen.getByTestId('InputRangeWithTextboxes');
+const inputRange = { min: 0, max: 50000 };
+const units = "km";
+let minInput = inputRange.min;
+const setMinInput = (value) => {
+  minInput = value;
+};
+let maxInput;
+const setMaxInput = (value) => {
+  maxInput = value;
+};
+const inputRangeValue = { min: 0, max: 50000 };
+const setInputRangeValue = (value) => {
+  inputRangeValue.min = value.min;
+  inputRangeValue.max = value.max;
+};
+
+describe("<InputRangeWithTextboxes />", () => {
+  test("it should mount", () => {
+    render(
+      <InputRangeWithTextboxes
+        inputRange={inputRange}
+        units={units}
+        minInput={minInput}
+        setMinInput={setMinInput}
+        maxInput={maxInput}
+        setMaxInput={setMaxInput}
+        inputRangeValue={inputRangeValue}
+        setInputRangeValue={setInputRangeValue}
+      />
+    );
+
+    const inputRangeWithTextboxes = screen.getByText(minInput);
 
     expect(inputRangeWithTextboxes).toBeInTheDocument();
   });
