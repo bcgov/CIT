4d35ffecac3aee4b4a6e0708a6788b5219ae86c7
justinmacaulay <justinkmacaulay@gmail.com>
2021-03-15 12:06:49 -0700

    missed debug import.

--- cit-api/pipeline/management/commands/import_data_sources.py
+++ cit-api/pipeline/management/commands/import_data_sources.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
 
 from pipeline.importers.data_sources import import_data_sources
-from pipeline.models.bc_assessment.bc_assessment_regional_district import BCAssessmentRegionalDistrict
 
 
 class Command(BaseCommand):
