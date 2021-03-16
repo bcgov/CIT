a0f6462986cd7bbbee3214fb6e981dba7df93b15
Paul Dittaro <pdittaro@gmail.com>
2021-03-16 11:37:15 -0700

    Migrations configuration

--- cit-api/entrypoint.prod.sh
+++ cit-api/entrypoint.prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT
+./wait-for-it.sh ${POSTGRES_HOST}:5432
 python3 manage.py migrate
 
-exec gunicorn --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread manage:app
+exec gunicorn --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread manage:main
