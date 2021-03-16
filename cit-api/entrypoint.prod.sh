941b417ba259bdce84e624440a824e89c6103b6c
Paul Dittaro <pdittaro@gmail.com>
2021-03-16 13:36:42 -0700

    Configure static files using Whitenoise

--- cit-api/entrypoint.prod.sh
+++ cit-api/entrypoint.prod.sh
@@ -3,4 +3,4 @@
 ./wait-for-it.sh ${POSTGRES_HOST}:5432
 python3 manage.py migrate
 
-exec gunicorn --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread manage:main
+exec gunicorn --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread admin.wsgi
