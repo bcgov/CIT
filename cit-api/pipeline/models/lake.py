dfaed13681b5f4a4cb898c02b852601a6decf8ad
justinmacaulay <justinkmacaulay@gmail.com>
2021-03-17 19:38:34 -0700

    fix for the coordinate issue.

--- cit-api/pipeline/models/lake.py
+++ cit-api/pipeline/models/lake.py
@@ -2,7 +2,7 @@ from django.contrib.gis.db import models
 from django.contrib.gis.db.models import MultiPolygonField
 from django.contrib.gis.geos import Point
 
-from pipeline.constants import BC_ALBERS_SRID
+from pipeline.constants import WGS84_SRID
 
 
 class Lake(models.Model):
@@ -10,8 +10,8 @@ class Lake(models.Model):
     NAME_FIELD = 'GNIS_NAME_1'
 
     name = models.CharField(max_length=127)
-    geom = models.MultiPolygonField(srid=BC_ALBERS_SRID, null=True)
-    geom_simplified = models.MultiPolygonField(srid=BC_ALBERS_SRID, null=True)
+    geom = models.MultiPolygonField(srid=WGS84_SRID, null=True)
+    geom_simplified = models.MultiPolygonField(srid=WGS84_SRID, null=True)
 
     class Meta:
         ordering = ("id", )
--- cit-api/pipeline/models/river.py
+++ cit-api/pipeline/models/river.py
@@ -2,7 +2,7 @@ from django.contrib.gis.db import models
 from django.contrib.gis.db.models import MultiPolygonField
 from django.contrib.gis.geos import Point
 
-from pipeline.constants import BC_ALBERS_SRID
+from pipeline.constants import WGS84_SRID
 
 
 class River(models.Model):
@@ -11,8 +11,8 @@ class River(models.Model):
 
     area_id = models.IntegerField(null=True, help_text="Original ID of data point")
     name = models.CharField(max_length=127)
-    geom = models.MultiPolygonField(srid=BC_ALBERS_SRID, null=True)
-    geom_simplified = models.MultiPolygonField(srid=BC_ALBERS_SRID, null=True)
+    geom = models.MultiPolygonField(srid=WGS84_SRID, null=True)
+    geom_simplified = models.MultiPolygonField(srid=WGS84_SRID, null=True)
 
     class Meta:
         ordering = ("id", )
