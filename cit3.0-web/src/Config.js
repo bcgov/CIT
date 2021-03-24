d32f50b8b7184cf5bf793fc8f1fcec387390d3ac
Paul Dittaro <pdittaro@gmail.com>
2021-03-24 10:27:02 -0700

    Create config file for imports

---
+++ cit3.0-web/src/Config.js
@@ -0,0 +1,10 @@
+const Config = {
+  apiUrl: window.env.apiUrl,
+  keycloakConfig: {
+    url: window.env.apiUrl,
+    realm: window.env.apiUrl,
+    clientId: window.env.apiUrl,
+  },
+};
+
+export default Config;
