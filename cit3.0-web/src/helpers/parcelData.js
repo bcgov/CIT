54eb7a6b2812735d8b635057de12f911875392fc
justinmacaulay <justinkmacaulay@gmail.com>
2021-03-25 12:32:56 -0700

    adding a new action to call parcelmap wfs

--- cit3.0-web/src/helpers/parcelData.js
+++ cit3.0-web/src/helpers/parcelData.js
@@ -1,4 +1,12 @@
 import axios from "axios";
+import proj4 from "proj4";
+import { SRID_DEF_3005 } from "./constants";
+
+const convertCoords = (coords) => {
+  console.log(coords);
+  proj4.defs("EPSG:3005", SRID_DEF_3005);
+  return proj4(proj4("EPSG:4326"), proj4("EPSG:3005"), [coords[1], coords[0]]);
+};
 
 export const getPID = async (siteId) =>
   axios
@@ -20,6 +28,20 @@ export const getPID = async (siteId) =>
 
 export async function getParcelData(pid) {
   const url = `https://openmaps.gov.bc.ca/geo/pub/WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW/wfs?service=wfs&version=2.0.0&request=GetFeature&outputFormat=application%2Fjson&TypeNames=pub:WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW&srsName=EPSG%3A3005&cql_filter=PID='${pid}'`;
+  return axios
+    .get(url)
+    .then((data) => {
+      if (data.data.features.length) {
+        return data;
+      }
+      return null;
+    })
+    .catch(() => null);
+}
+
+export async function getParcelDataNoAddress(coords) {
+  const convertedCoords = convertCoords(coords);
+  const url = `https://openmaps.gov.bc.ca/geo/pub/WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW/wfs?service=wfs&version=2.0.0&request=GetFeature&outputFormat=application%2Fjson&TypeNames=pub:WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW&srsName=EPSG%3A4326&cql_filter=INTERSECTS(SHAPE,POINT(${convertedCoords[0]} ${convertedCoords[1]}))`;
   return axios
     .get(url)
     .then((data) => {
