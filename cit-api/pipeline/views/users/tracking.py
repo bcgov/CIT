dbc546ff9fcb326b34725b94e151032db48df3fe
Zac Pez <Zachary.Pez@nttdata.com>
2021-03-29 14:26:10 -0700

    fix: swagger param issues

--- cit-api/pipeline/views/users/tracking.py
+++ cit-api/pipeline/views/users/tracking.py
@@ -8,16 +8,16 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
 
-user_id_param = openapi.Parameter('id', in_=openapi.IN_QUERY,
-                                  description='Existing User ID',
-                                  type=openapi.TYPE_INTEGER, required=True)
-report_url_param = openapi.Parameter('id', in_=openapi.IN_QUERY,
-                                     description='Report URL viewed',
-                                     type=openapi.TYPE_STRING, required=True)
+# user_id_param = openapi.Parameter('id', in_=openapi.IN_QUERY,
+#                                   description='Existing User ID',
+#                                   type=openapi.TYPE_INTEGER, required=True)
+# report_url_param = openapi.Parameter('id', in_=openapi.IN_QUERY,
+#                                      description='Report URL viewed',
+#                                      type=openapi.TYPE_STRING, required=True)
 
-@swagger_auto_schema(manual_parameters=[user_id_param], method='post',
-                      responses={status.HTTP_200_OK: UserTrackingSerializer()})
-@api_view(methods=['post'])
+# @swagger_auto_schema(manual_parameters=[user_id_param], method='post',
+#                       responses={status.HTTP_200_OK: UserTrackingSerializer()})
+# @api_view()
 class UserTrackingView(generics.CreateAPIView):
     model=UserTracking
     serializer_class = UserTrackingSerializer
