b0bdea89751b2128c44cddb05f9c903ea071d1fd
justinmacaulay <justinkmacaulay@gmail.com>
2021-03-29 08:21:12 -0700

    small fixes

--- cit3.0-web/src/components/Page/AddOpportunity/AddOpportunity.js
+++ cit3.0-web/src/components/Page/AddOpportunity/AddOpportunity.js
@@ -74,7 +74,6 @@ export default function AddOpportunity() {
     "Please confirm this is the property you want to list as an investment opportunity in your community";
 
   const setParcelData = async (id) => {
-    console.log("wrong function");
     const pid = await getPID(id);
     dispatch(setPID(pid));
     if (pid) {
@@ -114,31 +113,28 @@ export default function AddOpportunity() {
 
   const setParcelDataNoAddress = async (noAddrCoords) => {
     const parcelData = await getParcelDataNoAddress(noAddrCoords);
-    if (noAddressFlag) {
-      console.log(parcelData);
-      if (parcelData) {
-        dispatch(setPID(parcelData.data.features[0].properties.PID));
-        dispatch(
-          setParcelOwner(parcelData.data.features[0].properties.OWNER_TYPE)
-        );
-        if (parcelData.data.features[0].properties.OWNER_TYPE !== "Private") {
-          setBlockContinue(false);
-        } else {
-          setBlockContinue(true);
-        }
-        dispatch(
-          setParcelSize(
-            Number(
-              // convert sqM to Acres
-              (
-                parcelData.data.features[0].properties.FEATURE_AREA_SQM *
-                0.000247105
-              ).toFixed(3)
-            )
-          )
-        );
-        dispatch(setGeometry(parcelData.data.features[0].geometry));
+    if (noAddressFlag && parcelData) {
+      dispatch(setPID([parcelData.data.features[0].properties.PID]));
+      dispatch(
+        setParcelOwner(parcelData.data.features[0].properties.OWNER_TYPE)
+      );
+      if (parcelData.data.features[0].properties.OWNER_TYPE !== "Private") {
+        setBlockContinue(false);
+      } else {
+        setBlockContinue(true);
       }
+      dispatch(
+        setParcelSize(
+          Number(
+            // convert sqM to Acres
+            (
+              parcelData.data.features[0].properties.FEATURE_AREA_SQM *
+              0.000247105
+            ).toFixed(3)
+          )
+        )
+      );
+      dispatch(setGeometry(parcelData.data.features[0].geometry));
     } else {
       dispatch(setGeometry({ coordinates: null }));
       dispatch(setParcelOwner(null));
@@ -231,6 +227,14 @@ export default function AddOpportunity() {
                     </Col>
                   </Row>
                 )}
+                {!address && coords[0] !== 54.1722 && (
+                  <Row>
+                    <Col>
+                      <h3>Lat: {coords[0]}</h3>
+                      <h3>Lon: {coords[1]}</h3>
+                    </Col>
+                  </Row>
+                )}
                 {parcelSize ? (
                   <Row>
                     <Col>
@@ -298,10 +302,7 @@ export default function AddOpportunity() {
           <Terms agreed={agreed} setAgreed={setAgreed} />
         </Row>
       </Container>
-      <ButtonRow
-        noContinue={!PID || blockContinue || !agreed}
-        onClick={goToNextPage}
-      />
+      <ButtonRow noContinue={blockContinue || !agreed} onClick={goToNextPage} />
     </>
   );
 }
