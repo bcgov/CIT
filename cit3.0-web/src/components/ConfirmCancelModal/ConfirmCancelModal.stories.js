2cdf23e2169ef4cf2292f8b54b4af97329220a0c
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-04-06 12:44:58 -0700

    Updated storybook story

--- cit3.0-web/src/components/ConfirmCancelModal/ConfirmCancelModal.stories.js
+++ cit3.0-web/src/components/ConfirmCancelModal/ConfirmCancelModal.stories.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ConfirmCancelModal from "./ConfirmCancelModal";
 
 export default {
-  title: "Mark as sold modal",
+  title: "Confirm or Cancel modal",
   component: ConfirmCancelModal,
 };
 
-export const Default = () => <ConfirmCancelModal />;
+export const Default = () => {
+  const [show, setShow] = useState(true);
+  return (
+    <ConfirmCancelModal
+      show={show}
+      handleClose={() => setShow(false)}
+      handleSubmit={() => setShow(false)}
+      body="Modal body text"
+      label="modal label text"
+    />
+  );
+};
