3f9332269ba23da4438f89b94a0d99ea36c62585
HypnoMama <sarahhermsen@gmail.com>
2021-04-06 13:04:46 -0700

    reset opportunity and set noAddressFlag to false before running data searches

--- cit3.0-web/src/components/AddMarker/AddMarker.js
+++ cit3.0-web/src/components/AddMarker/AddMarker.js
@@ -6,47 +6,31 @@ import {
   getProximityData,
   getAddressFromPoint,
 } from "../../helpers/resourceCalls";
-import {
-  setGeometry,
-  setParcelOwner,
-  setParcelSize,
-  setPID,
-  setSiteId,
-  resetOpportunity,
-} from "../../store/actions/opportunity";
+import { setSiteId, resetOpportunity } from "../../store/actions/opportunity";
 
 export default function AddLocationMarker(props) {
   const [positions, setPositions] = useState([]);
   const dispatch = useDispatch();
 
   useMapEvent("click", async (e) => {
-    /* set current data to null in case data is not returned for a coord */
-    await props.setAddress("");
+    /* reset opportunity data and ensure noAddressFlag is false */
     dispatch(resetOpportunity());
-    // await dispatch(setParcelOwner(""));
-    // await dispatch(setGeometry(null));
-    // await dispatch(setParcelSize(null));
-    // await dispatch(setPID(""));
-    // await dispatch(setSiteId(null));
+    props.setNoAddressFlag(false);
     /// ///////////////////
-    await setPositions([e.latlng]);
-    await props.setCoords([e.latlng.lat, e.latlng.lng]);
+    setPositions([e.latlng]);
+    props.setCoords([e.latlng.lat, e.latlng.lng]);
     try {
       const addressDataFromPoint = await getAddressFromPoint([
         e.latlng.lat,
         e.latlng.lng,
       ]);
       if (addressDataFromPoint.data.properties.siteID) {
-        console.log("we have site id");
         dispatch(setSiteId(addressDataFromPoint.data.properties.siteID));
         props.setAddress(addressDataFromPoint.data.properties.fullAddress);
       }
     } catch (error) {
-      console.log("setting No address flag");
       props.setNoAddressFlag(true);
     } finally {
-      console.log("finally");
-
       const proximity = await getProximityData(props.resourceIds, [
         e.latlng.lat,
         e.latlng.lng,
