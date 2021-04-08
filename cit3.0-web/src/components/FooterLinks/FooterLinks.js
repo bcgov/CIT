9289d9e590d39448f00fe7e29ea30c0a2e809926
HypnoMama <sarahhermsen@gmail.com>
2021-04-08 16:50:05 -0700

    add Footer with button to link Dashboard with Investment Search page

---
+++ cit3.0-web/src/components/FooterLinks/FooterLinks.js
@@ -0,0 +1,54 @@
+import "./FooterLinks.scss";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { Row, Col } from "react-bootstrap";
+import { Button } from "shared-components";
+import { useHistory } from "react-router-dom";
+
+export default function FooterLinks({ type }) {
+  const history = useHistory();
+
+  const searchPage = {
+    heading: "Promote investment properties in your community",
+    subText:
+      "Are you working for a community and would like to promote local investment opportunities such as the ones you can discover on this site?  Click here to start adding your Community's opportunities.",
+    buttonText: "Add a Listing",
+    link: "/dashboard",
+  };
+
+  const AddOppPage = {
+    heading: "View Community Investment Opportunities in B.C.",
+    subText:
+      "Would you like to see all the investment opportunities in your region or find inspiration for a new listing?  View the Investment Opportunities Tool.",
+    buttonText: "Search Opportunities",
+    link: "/search",
+  };
+  const [data] = useState(type === "search-page" ? searchPage : AddOppPage);
+
+  return (
+    <Row className="footer-main">
+      <Col sm={9}>
+        <h3 className="pb-2">{data.heading}</h3>
+        <p>{data.subText}</p>
+      </Col>
+      <Col sm={3} className="d-flex justify-content-center align-items-center">
+        <Button
+          onClick={() => history.push(data.link)}
+          styling="button-yellow primary btn"
+          label={data.buttonText}
+        />
+      </Col>
+    </Row>
+  );
+}
+
+FooterLinks.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+const searchPage = {
+  heading: "",
+  subText: "",
+  buttonText: "",
+  link: "",
+};
