295aec994dd0da61ba2144881f6d4ac7185a9e3c
Zac Pez <Zachary.Pez@nttdata.com>
2021-04-09 12:05:09 -0700

    add: print/email links to opportunity view page

--- cit3.0-web/src/components/Page/OpportunityPage/OpportunityPage.js
+++ cit3.0-web/src/components/Page/OpportunityPage/OpportunityPage.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import Proptypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useHistory } from "react-router-dom";
-import { Button, Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { Button } from "shared-components";
 import OpportunityView from "../../OpportunityView/OpportunityView";
 import {
   setOpportunity,
@@ -10,13 +11,14 @@ import {
   resetOpportunity,
 } from "../../../store/actions/opportunity";
 import OpportunityFactory from "../../../store/factory/OpportunityFactory";
-import styles from "./OpportunityPage.module.css";
 
 const OpportunityPage = ({ id }) => {
   const history = useHistory();
   const location = useLocation();
   const dispatch = useDispatch();
   const opportunity = useSelector((state) => state.opportunity);
+  const opportunityName = useSelector((state) => state.opportunity.name);
+
   useEffect(() => {
     let opId = id;
     if (!id) {
@@ -37,16 +39,74 @@ const OpportunityPage = ({ id }) => {
     e.preventDefault();
   };
 
+  // Copy the visually hidden textarea
+  const copyLink = () => {
+    document.getElementById("link").select();
+    document.execCommand("copy");
+  };
+
+  // Use broswer print method
+  const openPdf = () => {
+    window.print();
+  };
+
+  // Open generic email with link
+  const emailLink = () => {
+    const subject = opportunityName;
+    let uri = "mailto:?subject=";
+    uri += encodeURIComponent(subject);
+    uri += "&body=";
+    uri += encodeURIComponent(window.location);
+    window.open(uri);
+  };
+
   return (
-    <div className={styles.OpportunityPage} data-testid="OpportunityPage">
-      <Container className="p-0">
-        <Button
-          className="a-tag mt-2 p-0"
-          onClick={resetState}
-          onKeyDown={resetState}
-        >
-          {"<<"} Return to Search
-        </Button>
+    <div data-testid="OpportunityPage">
+      <Container className="p-0 mt-3">
+        <Row className="no-print">
+          <Col className="align-self-end">
+            <Button
+              label="Go Back to Search"
+              styling="bcgov-normal-blue btn px-4"
+              onClick={resetState}
+              onKeyDown={resetState}
+            />
+            <textarea
+              readOnly
+              id="link"
+              className="visually-hidden"
+              value={window.location}
+            />
+          </Col>
+          <Col>
+            <div className="d-flex flex-grow-1 justify-content-end">
+              <div className="d-flex flex-column">
+                <button
+                  type="button"
+                  className="a-tag"
+                  onClick={() => copyLink()}
+                >
+                  Copy Listing Link
+                </button>
+                <button
+                  type="button"
+                  className="a-tag"
+                  onClick={() => emailLink()}
+                >
+                  Email Listing Link
+                </button>
+                <button
+                  type="button"
+                  className="a-tag"
+                  onClick={() => openPdf()}
+                >
+                  View Listing PDF
+                </button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <hr className="my-3 hr-bold no-print" />
       </Container>
       <OpportunityView view="all" />
     </div>
