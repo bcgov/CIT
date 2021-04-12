25e5f80069d16e2eb53779d845a4cf1502b108ce
Paul Dittaro <pdittaro@gmail.com>
2021-04-12 15:12:03 -0700

    Add storage account for data pipeline

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -1,7 +1,6 @@
 ##############################
 ## Azure Container Registry ##
 ##############################
-
 resource "azurerm_container_registry" "default" {
   name                = "acraf6z1jmd"
   resource_group_name = var.azure_resource_group
@@ -19,21 +18,34 @@ resource "azurerm_container_registry" "default" {
 ##############################
 ## Github Repo Secrets      ##
 ##############################
-
 resource "github_actions_secret" "registry_username" {
   repository      = var.github_repository
   secret_name     = "REGISTRY_USERNAME"
   plaintext_value = azurerm_container_registry.default.admin_username
 }
-
 resource "github_actions_secret" "registry_password" {
   repository      = var.github_repository
   secret_name     = "REGISTRY_PASSWORD"
   plaintext_value = azurerm_container_registry.default.admin_password
 }
-
 resource "github_actions_secret" "registry_login_server" {
   repository      = var.github_repository
   secret_name     = "REGISTRY_LOGIN_SERVER"
   plaintext_value = azurerm_container_registry.default.login_server
+}
+
+##############################
+## Data Pipeline Storage    ##
+##############################
+resource "azurerm_storage_account" "data" {
+  name                     = "citpipelinedata"
+  resource_group_name      = var.azure_resource_group
+  location                 = var.azure_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_storage_container" "pipeline" {
+  name                  = "data"
+  storage_account_name  = azurerm_storage_account.data.name
+  container_access_type = "private"
 }
