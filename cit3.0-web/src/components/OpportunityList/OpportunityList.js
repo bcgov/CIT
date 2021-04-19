1a0ec5e20c964a768a9abf99bc25e6de12247915
Zac Pez <Zachary.Pez@nttdata.com>
2021-04-19 16:44:20 -0700

    update: public listing card style and data shown

--- cit3.0-web/src/components/OpportunityList/OpportunityList.js
+++ cit3.0-web/src/components/OpportunityList/OpportunityList.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 import { v4 } from "uuid";
 import OpportunityFactory from "../../store/factory/OpportunityFactory";
 
@@ -6,13 +7,21 @@ export default function OpportunityList({
   opportunities,
   component: Component,
 }) {
+  const options = useSelector((state) => state.options);
   return (
     <>
       {opportunities.length
         ? opportunities.map((opp) => {
             const opportunity = OpportunityFactory.createStateFromResponse(opp);
             const ListItem = Component();
-            return <ListItem key={v4()} opportunity={opportunity} publicView />;
+            return (
+              <ListItem
+                key={v4()}
+                options={options}
+                opportunity={opportunity}
+                publicView
+              />
+            );
           })
         : null}
     </>
--- cit3.0-web/src/components/OpportunityListItem/OpportunityListItem.js
+++ cit3.0-web/src/components/OpportunityListItem/OpportunityListItem.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Link, NavLink, useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import LinesEllipsis from "react-lines-ellipsis";
+import NumberFormat from "react-number-format";
 import Map from "../Map/Map";
 import {
   determineStatusTextColour,
@@ -15,7 +16,12 @@ import {
 } from "../../store/actions/opportunity";
 import OpportunityFactory from "../../store/factory/OpportunityFactory";
 
-const OpportunityListItem = ({ opportunity, publicView, handleModalOpen }) => {
+const OpportunityListItem = ({
+  options,
+  opportunity,
+  publicView,
+  handleModalOpen,
+}) => {
   const history = useHistory();
   const dispatch = useDispatch();
   const goToEditListing = () => {
@@ -77,6 +83,13 @@ const OpportunityListItem = ({ opportunity, publicView, handleModalOpen }) => {
     );
   };
 
+  const saleOrLease = () => {
+    const propertyStatus = options.propertyStatuses.find(
+      (s) => s.code === opportunity.userInfo.saleOrLease.value
+    ).name;
+    return propertyStatus !== "Both" ? propertyStatus : "Sale or Lease";
+  };
+
   return (
     <div key={opportunity.id} className="opportunity-table-row w-100">
       <Row
@@ -95,9 +108,47 @@ const OpportunityListItem = ({ opportunity, publicView, handleModalOpen }) => {
             />
           </div>
         </Col>
-        <Col>
+        <Col className="d-flex flex-column">
           <Row>
-            <Col>{opportunity ? getAddress(opportunity.address) : ""}</Col>
+            <Col>
+              <b>{opportunity ? getAddress(opportunity.address) : ""}</b>
+            </Col>
+            <Col
+              style={{
+                marginRight: "0.5rem",
+              }}
+            >
+              <div className="d-flex flex-row flex-wrap align-content-end">
+                {opportunity.siteInfo.parcelSize.value ? (
+                  <p className="border--pill">
+                    Parcel Size:{" "}
+                    <NumberFormat
+                      displayType="text"
+                      value={opportunity.siteInfo.parcelSize.value}
+                      suffix={` acres`}
+                      decimalScale={3}
+                      thousandSeparator={
+                        isNaN(opportunity.siteInfo.parcelSize.value)
+                          ? false
+                          : ","
+                      }
+                    />
+                  </p>
+                ) : null}
+                {opportunity.userInfo.currentZone.value && options ? (
+                  <p className="border--pill">{`Zoning: ${
+                    options.landUseZoning.find(
+                      (s) => s.code === opportunity.userInfo.currentZone.value
+                    ).name
+                  }`}</p>
+                ) : null}
+                {opportunity.userInfo.saleOrLease.value && options ? (
+                  <p className="border--pill">{`${saleOrLease()}`}</p>
+                ) : null}
+              </div>
+            </Col>
+          </Row>
+          <Row className="flex-grow-1">
             {!publicView ? (
               <>
                 <Col>{formatDate(opportunity.dateCreated)}</Col>
@@ -107,42 +158,71 @@ const OpportunityListItem = ({ opportunity, publicView, handleModalOpen }) => {
                 <Col>{determineActions(opportunity)}</Col>
               </>
             ) : (
-              <Col className="d-flex align-items-end justify-content-end mr-1">
+              <Col>
                 {publicView && (
-                  <Link to={opportunity.link}>View property details</Link>
+                  <Row className="h-100">
+                    <Col
+                      style={{
+                        paddingTop: "0.5rem",
+                        paddingBottom: "0.5rem",
+                      }}
+                    >
+                      <LinesEllipsis
+                        className="note"
+                        text={opportunity.userInfo.opportunityDescription.value}
+                        maxLine="4"
+                        ellipsis="..."
+                        trimRight
+                        basedOn="letters"
+                      />
+                    </Col>
+                    <Col
+                      style={{
+                        alignSelf: "flex-end",
+                        paddingBottom: "0.5rem",
+                        marginRight: "0.5rem",
+                      }}
+                      sm={6}
+                      md={6}
+                      lg={4}
+                      className="text-right"
+                    >
+                      <Link to={opportunity.link}>View property details</Link>
+                    </Col>
+                  </Row>
                 )}
               </Col>
             )}
           </Row>
-
-          {opportunity.publicNote ? (
-            <Row>
-              <Col className="pl-0">
-                <b className="note-title">
-                  Comment from {opportunity.lastAdmin}:
-                </b>
-                <LinesEllipsis
-                  className="note pr-3"
-                  text={opportunity.publicNote}
-                  maxLine="3"
-                  ellipsis="..."
-                  trimRight
-                  basedOn="letters"
-                />
-              </Col>
-            </Row>
-          ) : null}
         </Col>
       </Row>
+
+      {opportunity.publicNote ? (
+        <Row>
+          <Col className="pl-0">
+            <b className="note-title">Comment from {opportunity.lastAdmin}:</b>
+            <LinesEllipsis
+              className="note pr-3"
+              text={opportunity.publicNote}
+              maxLine="3"
+              ellipsis="..."
+              trimRight
+              basedOn="letters"
+            />
+          </Col>
+        </Row>
+      ) : null}
     </div>
   );
 };
 
 OpportunityListItem.defaultProps = {
+  options: {},
   handleModalOpen: () => {},
 };
 
 OpportunityListItem.propTypes = {
+  options: PropTypes.shape(),
   opportunity: PropTypes.shape().isRequired,
   handleModalOpen: PropTypes.func,
   publicView: PropTypes.bool.isRequired,
