5d272d8788791ccf693f39ce6596907a0c9057c1
HypnoMama <31192638+HypnoMama@users.noreply.github.com>
2021-04-19 10:44:22 -0700

    fix typo on dashboard page (first li was missing text), new text to siteInfo warning page (#633)
    * fix typo on dashboard page (first li was missing text)
    
    * add new text to warning
    
    * move p to li, change tagline, cit-318
    
    * remove comment

--- cit3.0-web/src/components/Page/EDODashboard/EDODashboard.js
+++ cit3.0-web/src/components/Page/EDODashboard/EDODashboard.js
@@ -173,7 +173,8 @@ export default function EDODashboard() {
           <li>
             Properties must be zoned for industrial, commercial, or agricultural
             use. Industrial properties of any size may be listed. Commercial and
-            agricultural
+            agricultural properties may be listed if they are at least 5 acres
+            in size.
           </li>
           <li>Land must be available for sale or lease.</li>
           <li>
@@ -193,15 +194,15 @@ export default function EDODashboard() {
                 Presence of provincially significant cultural or natural
                 heritage features
               </li>
+              <li>
+                Listings must be approved by either a municipality, regional
+                district, electoral area, province, First Nation or a regional
+                economic development organization, and from the Chief
+                Administrative Officer or their delegate.
+              </li>
             </ul>
           </li>
         </ul>
-        <p className="dashboard-text mb-4">
-          Listings must be approved by either a municipality, regional district,
-          electoral area, province, First Nation or a regional economic
-          development organization, and from the Chief Administrative Officer or
-          their delegate.
-        </p>
         <div className="add-opportunity-button">{addOpportunityButton}</div>
         <hr ref={resultRef} />
         {markAsSoldStatus === "Error" ? (
