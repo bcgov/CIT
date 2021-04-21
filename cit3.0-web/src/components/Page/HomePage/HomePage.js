113aa0168bac0453dd9fd6ccd31b58b5e45e31a7
HypnoMama <sarahhermsen@gmail.com>
2021-04-21 08:59:33 -0700

    add businessto bceid, make bold

--- cit3.0-web/src/components/Page/HomePage/HomePage.js
+++ cit3.0-web/src/components/Page/HomePage/HomePage.js
@@ -182,11 +182,12 @@ export default function HomePage() {
                   </p>
                   <p>
                     To create a listing, authorized representatives will be
-                    required to log-in with a BCeID. A one-time verification
-                    process will confirm your status as an authorized community
-                    representative community representative when you create your
-                    first listing. All listings will be reviewed prior to
-                    publication and will go live within 3-5 business days.
+                    required to log-in with a <b>Business BCeID</b>. A one-time
+                    verification process will confirm your status as an
+                    authorized community representative community representative
+                    when you create your first listing. All listings will be
+                    reviewed prior to publication and will go live within 3-5
+                    business days.
                   </p>
                 </Row>
               </Col>
