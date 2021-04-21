fc1bf8cf79a6e5d0870e5d280bbd47dd2d55df7f
Zac Pez <Zachary.Pez@nttdata.com>
2021-04-21 10:11:29 -0700

    updated: closed labels

--- cit3.0-web/src/components/OpportunityTable/OpportunityTable.test.js
+++ cit3.0-web/src/components/OpportunityTable/OpportunityTable.test.js
@@ -68,7 +68,7 @@ describe("OpportunityTable", () => {
     expect(getByText(/Not completed/i)).toHaveClass("status-text-red");
   });
 
-  it("displays the option to mark as closed/won for published opportunities", () => {
+  it("displays the option to mark as closed for published opportunities", () => {
     const { getByText } = render(
       <Provider store={store}>
         <Router history={history}>
@@ -77,6 +77,6 @@ describe("OpportunityTable", () => {
       </Provider>
     );
 
-    expect(getByText(/Closed/i).textContent).toBe("Closed/Won");
+    expect(getByText(/Closed/i).textContent).toBe("Closed");
   });
 });
