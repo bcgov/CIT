08724bb0f0425fe5be8db741a49897f913aaa7bf
HypnoMama <sarahhermsen@gmail.com>
2021-04-22 15:30:51 -0700

    refactor citHome to remove option to select community - only send to generic report with no filters set

--- cit3.0-web/src/components/Page/citHome/citHome.js
+++ cit3.0-web/src/components/Page/citHome/citHome.js
@@ -1,21 +1,14 @@
 import "./citHome.css";
-import { Container, Row, Col, Button, Modal } from "react-bootstrap";
+import { Container, Row, Col, Modal } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import "../HomePage/HomePage.scss";
 import { Button as SharedButton } from "shared-components";
-import { BsSearch } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Typeahead } from "react-bootstrap-typeahead";
 import { useKeycloakWrapper } from "../../../hooks/useKeycloakWrapper";
 import useConfiguration from "../../../hooks/useConfiguration";
-import { toKebabCase } from "../../../helpers/helpers";
 
 export default function citHome() {
-  const [selectedPlace, setSelectedPlace] = useState(null);
-  const [places, setPlaces] = useState(null);
-  const [communities, setCommunities] = useState(null);
-  const [regional, setRegional] = useState(null);
   const history = useHistory();
   const keycloak = useKeycloakWrapper();
   const [loggedInWithIdir] = useState(keycloak.idp === "idir");
@@ -36,54 +29,16 @@ export default function citHome() {
   const publicUrl = "/cit-dashboard/public";
   const privateUrl = "/cit-dashboard/internal";
 
-  useEffect(() => {
-    axios.get("/api/opportunity/options").then((data) => {
-      const commNames = data.data.communities.map((comm) => comm.place_name);
-      setCommunities(commNames);
-      const regNames = data.data.regionalDistricts.map((dist) => dist.name);
-      setRegional(regNames);
-      setPlaces([...commNames, ...regNames]);
-    });
-  }, []);
-
-  const typeOfSelected = (place) => {
-    if (communities.find((c) => c === place)) {
-      return "community";
-    }
-    if (regional.find((r) => r === place)) {
-      return "regionalDistrict";
-    }
-    return null;
-  };
-
   const handlePublic = () => {
-    if (selectedPlace) {
-      const type = typeOfSelected(selectedPlace);
-      history.push(`${publicUrl}?${type}=${toKebabCase(selectedPlace)}`);
-    } else {
-      history.push(publicUrl);
-    }
+    history.push(publicUrl);
   };
 
   const handleLogin = () => {
     // login with IDIR only and redirect to private report
-    let loginWithIdir;
-    if (selectedPlace) {
-      const type = typeOfSelected(selectedPlace);
-      loginWithIdir = keycloak.obj.createLoginUrl({
-        idpHint: "idir",
-        redirectUri: encodeURI(
-          `${configuration.baseUrl}${privateUrl}?${type}=${toKebabCase(
-            selectedPlace
-          )}`
-        ),
-      });
-    } else {
-      loginWithIdir = keycloak.obj.createLoginUrl({
-        idpHint: "idir",
-        redirectUri: encodeURI(`${configuration.baseUrl}${privateUrl}`),
-      });
-    }
+    const loginWithIdir = keycloak.obj.createLoginUrl({
+      idpHint: "idir",
+      redirectUri: encodeURI(`${configuration.baseUrl}${privateUrl}`),
+    });
     window.location.href = loginWithIdir;
   };
 
@@ -118,55 +73,11 @@ export default function citHome() {
           </Col>
         </Row>
         <Row>
-          <Col className="box mr-3 cit-box">
-            <Row>
-              <Col>
-                <h3>View my community or region</h3>
-                <p className="my-2">
-                  Community and regional profiles include socioeconomic data,
-                  infrastructure and community assets to provide a sense of what
-                  a community is like – and how it is changing.{" "}
-                </p>
-                <>
-                  {places ? (
-                    <div className="w-100 mt-4 comm-search-div">
-                      <Typeahead
-                        id="search-by-community"
-                        size="large"
-                        onChange={(selected) => {
-                          setSelectedPlace(selected[0]);
-                          handleShow();
-                        }}
-                        options={places}
-                      />
-                      <Button
-                        className="search-glass-box"
-                        variant="outline-secondary"
-                      >
-                        <BsSearch />
-                      </Button>
-                    </div>
-                  ) : null}
-                </>
-              </Col>
-            </Row>
-            <Row className="pb-0 d-flex justify-content-end">
-              <Col sm={3} className="svg-box pt-3">
-                <img
-                  className="add-opp-img"
-                  src="/images/house_CITHOME.svg"
-                  height="100%"
-                  width="100%"
-                  alt="cit home community search box"
-                />
-              </Col>
-            </Row>
-          </Col>
-          <Col className="box ml-3 cit-box explore-box corner">
+          <Col className="box cit-box explore-box corner">
             <Row>
               <Col>
                 <h3>Discover insights and patterns among B.C. communities</h3>
-                <p className="my-2">
+                <p className="my-3">
                   Find all communities in B.C. with particular characteristics –
                   whether you’re interested in economic health, access to
                   education and health care, connectivity, infrastructure, or
@@ -174,16 +85,14 @@ export default function citHome() {
                 </p>
               </Col>
             </Row>
-            <Row>
-              <Col className="mt-2 d-flex justify-content-end">
+            <Row className="mb-0 pr-0 mt-3">
+              <Col className="mt-2">
                 <SharedButton
                   onClick={handleShow}
                   styling="home-buttons explore-button"
                   label="Explore B.C. Communities"
                 />
               </Col>
-            </Row>
-            <Row className="mb-0 pr-0 mt-3 d-flex justify-content-end">
               <Col sm={6} className="p-0 mb-0 svg-box">
                 <img
                   className="add-opp-img"
