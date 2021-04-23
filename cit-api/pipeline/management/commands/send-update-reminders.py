a215dd48f01f17aca687197be3879f3f2f294af0
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-04-23 11:49:31 -0700

    Refactored to send a single email per EDO for stale opportunities

--- cit-api/pipeline/management/commands/send-update-reminders.py
+++ cit-api/pipeline/management/commands/send-update-reminders.py
@@ -13,20 +13,25 @@ class Command(BaseCommand):
         if email_enabled not in ["True", "true"]:
             print("Email notifications are not currently enabled")
             return
-        stale_time = now() - timedelta(hours=1)
+        stale_time = now() - timedelta(seconds=1)
         print("Notifying opportunities last modified before: " + str(stale_time))
         stale_opportunities = Opportunity.objects.filter(date_updated__lte=stale_time,deleted=False,approval_status="PUBL").exclude(user_id__email__in=['', 'Unknown'])
         print("Total stale opportunities to be notified:" + str(stale_opportunities.count()))
 
+        email_opportunity_mapping = {}
         for opportunity in stale_opportunities:
-            email_result = send_reminder_email(opportunity)
-            if email_result.status_code == 201:
-                print("Email sent successfully for opportunity ID " + str(opportunity.id))
+            print("Sending notification for opportunity with ID " + str(opportunity.id))
+            edo_email = get_edo_email_for_opportunity(opportunity.id)
+            if edo_email in email_opportunity_mapping:
+                email_opportunity_mapping[edo_email].append(opportunity)
             else:
-                print(email_result.text)
-                print("Unable to successfully send email for opportunity ID " + str(opportunity.id))  
+                email_opportunity_mapping[edo_email] = [opportunity]
 
-def send_reminder_email(opportunity):
+        for email in email_opportunity_mapping:
+            print(email)
+            email_result = send_reminder_email(email, email_opportunity_mapping[email])
+
+def send_reminder_email(email, opportunity_list):
     token_request_body = {'grant_type': 'client_credentials'}
     response = requests.post(os.environ.get("EMAIL_AUTH_HOST") + "/auth/realms/jbd6rnxw/protocol/openid-connect/token", data=token_request_body, auth=HTTPBasicAuth(os.environ.get("EMAIL_CLIENT_ID"),os.environ.get("EMAIL_CLIENT_SECRET")))       
     if response.status_code == 200:
@@ -40,22 +45,32 @@ def send_reminder_email(opportunity):
             email_config = {
                 "bcc": [],
                 "bodyType": "html",
-                "body": construct_email_body(opportunity),
+                "body": construct_email_body(opportunity_list),
                 "cc": [],
                 "delayTS": 0,
                 "encoding": "utf-8",
                 "from": os.environ.get("EMAIL_SENDING_ADDRESS"),
                 "priority": "normal",
-                "subject": "Your Opportunity has not been Modified for 90 Days",
-                "to": [get_edo_email_for_opportunity(opportunity.id)],
+                "subject": "Your have Opportunities that have not been Modified for 90 Days",
+                "to": [email],
                 "tag": "CIT_Admin_Notification",
             }
             email_config_json = json.dumps(email_config)
             response = requests.post(os.environ.get("EMAIL_SERVICE_HOST") + "/api/v1/email", data=email_config_json, headers=headers)
         return response
 
-def construct_email_body(opportunity):
-    return "<h1>Your opportunity has not been modified for 90 days.</h1><p>Please click here to view your dashboard, and update any information that has changed: " + build_full_link() + "</p>"
+def construct_email_body(opportunity_list):
+    opportunity_email_string = ""
+    for opportunity in opportunity_list:
+        opportunity_email_string = opportunity_email_string + "<p>"    
+        if opportunity.opportunity_address != "":
+            opportunity_email_string = opportunity_email_string + opportunity.opportunity_address + " - "
+        else:
+            opportunity_email_string = opportunity_email_string + "No address - "
+        opportunity_email_string = opportunity_email_string + "Created on " + str(opportunity.date_created)
+        opportunity_email_string = opportunity_email_string + "</p>"
+
+    return "<h1>You have opportunities that have not been modified for 90 days.</h1><p>These opportunities must be reviewed:</p>" + opportunity_email_string + "<p>Please click here to view your dashboard, and update any information that has changed: " + build_full_link() + "</p>"
 
 def get_edo_email_for_opportunity(id):
     opportunity = Opportunity.objects.get(id=id)
