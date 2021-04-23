0f1e1add4569bfa0142d9adcdcd87679f848ed32
Paul Dittaro <pdittaro@gmail.com>
2021-04-23 13:18:02 -0700

    Update PBI ids

--- terraform/cit-prod/frontend.tf
+++ terraform/cit-prod/frontend.tf
@@ -12,19 +12,20 @@ resource "azurerm_app_service" "frontend" {
   }
 
   app_settings = {
-    DOCKER_REGISTRY_SERVER_URL          = "https://${data.terraform_remote_state.shared.outputs.acr_name}.azurecr.io"
-    DOCKER_REGISTRY_SERVER_USERNAME     = data.terraform_remote_state.shared.outputs.acr_admin
-    DOCKER_REGISTRY_SERVER_PASSWORD     = data.terraform_remote_state.shared.outputs.acr_password
-    DOCKER_ENABLE_CI                    = true
-    REACT_APP_API_BASE_URL              = "https://${azurerm_app_service.backend.default_site_hostname}"
-    REACT_APP_KEYCLOAK_URL              = "https://oidc.gov.bc.ca/auth/"
-    REACT_APP_KEYCLOAK_CLIENT           = "cit-prod"
-    REACT_APP_KEYCLOAK_REALM            = "fyof530u"
-    REACT_APP_POWER_BI_GROUP_ID         = "9bd828a8-9256-45f7-80ab-f39b8f647de0"
-    REACT_APP_POWER_BI_REPORT_ID        = "3a848d28-934f-4e08-be41-b4acd90f616a"
-    REACT_APP_GEOCODER_API_KEY          = var.geocoder_key
-    REACT_APP_BC_ROUTE_PLANNER_API_KEY  = var.router_key
-    REACT_APP_SNOWPLOW_COLLECTOR        = "spt.apps.gov.bc.ca"
+    DOCKER_REGISTRY_SERVER_URL            = "https://${data.terraform_remote_state.shared.outputs.acr_name}.azurecr.io"
+    DOCKER_REGISTRY_SERVER_USERNAME       = data.terraform_remote_state.shared.outputs.acr_admin
+    DOCKER_REGISTRY_SERVER_PASSWORD       = data.terraform_remote_state.shared.outputs.acr_password
+    DOCKER_ENABLE_CI                      = true
+    REACT_APP_API_BASE_URL                = "https://${azurerm_app_service.backend.default_site_hostname}"
+    REACT_APP_KEYCLOAK_URL                = "https://oidc.gov.bc.ca/auth/"
+    REACT_APP_KEYCLOAK_CLIENT             = "cit-prod"
+    REACT_APP_KEYCLOAK_REALM              = "fyof530u"
+    REACT_APP_POWER_BI_GROUP_ID           = "0399d295-4354-4955-8ed9-68709eb5e7b5"
+    REACT_APP_POWER_BI_REPORT_ID_PUBLIC   = "3776791c-6ded-4300-8833-5040e3b948c0"
+    REACT_APP_POWER_BI_REPORT_ID_INTERNAL = "3fb90e72-006c-4d43-8e54-8d626510761c"
+    REACT_APP_GEOCODER_API_KEY            = var.geocoder_key
+    REACT_APP_BC_ROUTE_PLANNER_API_KEY    = var.router_key
+    REACT_APP_SNOWPLOW_COLLECTOR          = "spt.apps.gov.bc.ca"
   }
 
   tags = {
