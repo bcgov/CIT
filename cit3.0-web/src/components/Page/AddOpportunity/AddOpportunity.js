dd1ad21e9b44e5d240b13c6e39dccd6c19ded29b
Zac Pez <Zachary.Pez@nttdata.com>
2021-04-23 15:45:24 -0700

    add: clear parcel info option

--- cit3.0-web/src/components/Page/AddOpportunity/AddOpportunity.js
+++ cit3.0-web/src/components/Page/AddOpportunity/AddOpportunity.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Button } from "shared-components";
 import { useEffect, useState } from "react";
 import v4 from "uuid";
+import { Callout } from "shared-components/build/components/callout/Callout";
+import NumberFormat from "react-number-format";
 import NavigationHeader from "../../Headers/NavigationHeader/NavigationHeader";
 import MapContainer from "../../MapContainer/MapContainer";
 import AddressSearchBar from "../../AddressSearchBar/AddressSearchBar";
@@ -30,6 +32,7 @@ import {
 import Radios from "../../FormComponents/Radios";
 import Terms from "../../Terms/Terms";
 import LoadingScreen from "../../LoadingScreen/LoadingScreen";
+import TextInput from "../../FormComponents/TextInput";
 
 export default function AddOpportunity() {
   const dispatch = useDispatch();
@@ -50,9 +53,6 @@ export default function AddOpportunity() {
   const parcelOwner = useSelector(
     (state) => state.opportunity.siteInfo.parcelOwnership.name
   );
-  const nearbyResources = useSelector(
-    (state) => state.opportunity.nearbyResources
-  );
   const municipality = useSelector(
     (state) => state.opportunity.municipality.name
   );
@@ -62,6 +62,7 @@ export default function AddOpportunity() {
   const [error, setError] = useState([]);
   const [agreed, setAgreed] = useState(false);
   const [noAddressFlag, setNoAddressFlag] = useState(false);
+  const [parcelInfo, setParcelInfo] = useState(false);
 
   // Handle ProximityData call still running and change of page
   const [proximityInProgress, setProximityInProgress] = useState(true);
@@ -180,44 +181,43 @@ export default function AddOpportunity() {
     setHasApproval(false);
     /// //////////////////////////
     const pid = await getPID(id);
+    let countedSQM = 0;
     dispatch(setPID(pid));
     if (pid) {
       pid.forEach(async (_pid) => {
         const parcelData = await getParcelData(_pid);
         if (parcelData) {
-          dispatch(
+          await dispatch(
             setParcelOwner(parcelData.data.features[0].properties.OWNER_TYPE)
           );
+          countedSQM += parcelData.data.features[0].properties.FEATURE_AREA_SQM;
+          await dispatch(setGeometry(parcelData.data.features[0].geometry));
           await dispatch(
             setParcelSize(
               Number(
                 // convert sqM to Acres
-                (
-                  parcelData.data.features[0].properties.FEATURE_AREA_SQM *
-                  0.000247105
-                ).toFixed(3)
+                (countedSQM * 0.000247105).toFixed(3)
               )
             )
           );
-          dispatch(setGeometry(parcelData.data.features[0].geometry));
-          if (!geometry) {
-            // There is no geometry registered for this PID, try a lng lat query.
-            setParcelInfoByCoords(coords);
-          }
         } else {
           // There is no Parcel response for this PID, try a lng lat query.
           setParcelInfoByCoords(coords);
         }
       });
     } else {
-      // There is no PID for the pacel to be queried, try a lng lat query.
+      // There is no PID for the parcel to be queried, try a lng lat query.
       setParcelInfoByCoords(coords);
     }
   };
 
   const setParcelDataNoAddress = async (noAddrCoords) => {
     // ensure previous parcel data is cleared
-    dispatch(resetOpportunity());
+    dispatch(setParcelSize(null));
+    dispatch(setParcelOwner(null));
+    dispatch(setGeometry(null));
+    // ensure hasApproval is false
+    setHasApproval(false);
     // reset coords with new coords
     dispatch(setCoords(noAddrCoords));
     // ensure hasApproval is false
@@ -259,6 +259,11 @@ export default function AddOpportunity() {
 
   const handleError = (message) => {
     setError([...error, message]);
+  };
+
+  const handleUpdateParcelInfo = () => {
+    dispatch(setGeometry(null));
+    setParcelInfo(true);
   };
 
   return (
@@ -335,21 +340,36 @@ export default function AddOpportunity() {
                     </Col>
                   </Row>
                 )}
-                {parcelSize ? (
-                  <Row>
+                <Row>
+                  {!parcelInfo ? (
                     <Col>
                       <PropertyInfo info={address} tag={false} />
-                      <p className="mb-0 mt-3 pb-0">
-                        Ownership: <b>{parcelOwner}</b>
-                      </p>
-                      <p className="mb-0 pb-0">
-                        Parcel Size:{" "}
-                        <b>{parcelSize ? parcelSize.toFixed(3) : null} acres</b>
-                      </p>
-                      <p>
-                        PID:{" "}
-                        <b>{PID && PID.length > 1 ? PID.join(", ") : PID}</b>
-                      </p>
+                      {parcelOwner ? (
+                        <p className="mb-0 mt-3 pb-0">
+                          Ownership: <b>{parcelOwner}</b>
+                        </p>
+                      ) : null}
+                      {parcelSize ? (
+                        <p className="mb-0 pb-0">
+                          Parcel Size: <b>{parcelSize.toFixed(3)} acres</b>
+                        </p>
+                      ) : null}
+                      {PID ? (
+                        <p>
+                          PID:{" "}
+                          <b>{PID && PID.length > 1 ? PID.join(", ") : PID}</b>
+                        </p>
+                      ) : null}
+                      {parcelSize ? (
+                        <div>
+                          <Callout
+                            text="If the parcel information populated here is
+  incorrect. Clear and enter new details."
+                            checkboxLabel="Enter new details"
+                            agreeCallout={handleUpdateParcelInfo}
+                          />
+                        </div>
+                      ) : null}
                       {parcelOwner === "Private" && (
                         <>
                           <PropertyInfo info="This land parcel or development opportunity resides on private land." />
@@ -381,8 +401,56 @@ export default function AddOpportunity() {
                         </>
                       )}
                     </Col>
-                  </Row>
-                ) : null}
+                  ) : (
+                    <div className="m-3 ">
+                      <TextInput
+                        heading="Parcel Ownership"
+                        notes="Please note the information here will appear publicly"
+                        placeholder="Private"
+                        rows={1}
+                        value={parcelOwner}
+                        handleChange={(_, value) =>
+                          dispatch(setParcelOwner(value))
+                        }
+                        name="owner"
+                      />
+                      <div className="d-flex flex-column w-100">
+                        <p className="mb-0">Parcel Size</p>
+                        <p className="mb-0" style={{ opacity: "0.5" }}>
+                          The lot size that will be shown publicly.
+                        </p>
+                        <div className="text-wrapper">
+                          <NumberFormat
+                            className="bcgov-text-input"
+                            type="text"
+                            displayType="input"
+                            decimalScale={3}
+                            value={parcelSize || 0}
+                            units="acres"
+                            onValueChange={(value) => {
+                              if (value.floatValue !== parcelSize) {
+                                dispatch(setParcelSize(value.floatValue));
+                              }
+                            }}
+                          />
+                        </div>
+                      </div>
+                      <TextInput
+                        heading="PID"
+                        notes="Enter a parcel number(s) separated by commas, if available"
+                        placeholder="32165785"
+                        rows={1}
+                        value={PID && PID.join(",")}
+                        handleChange={(_, value) => {
+                          if (PID.join(",") !== value) {
+                            dispatch(setPID(value.split(",")));
+                          }
+                        }}
+                        name="owner"
+                      />
+                    </div>
+                  )}
+                </Row>
               </Col>
             </Row>
           </Col>
