194fea6cca1fa035182e10c9173067ed0ddc0018
Zac Pez <zacpez@gmail.com>
2021-05-06 14:55:57 -0700

    add: powerbi role for IDIR users, paired with auto-apply on signup via keycloak (#709)
    Co-authored-by: Zac Pez <Zachary.Pez@nttdata.com>

--- cit3.0-web/src/constants/roles.js
+++ cit3.0-web/src/constants/roles.js
@@ -5,6 +5,7 @@ const Roles = {
   SUPER_ADMINISTRATOR: "SuperAdministrator",
   SYSTEM_ADMINISTRATOR: "IDIR",
   ECONOMIC_DEVELOPMENT_OFFICER: "BCeID",
+  POWER_BI_VIEWER: "PowerBI",
 };
 
 export default Roles;
