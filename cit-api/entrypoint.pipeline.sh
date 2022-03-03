b3b9773116b4378f023902859dc68a0ce6f7def4
Jessica Stratton <jessicajstratton@outlook.com>
2022-03-03 09:28:38 -0800

    added in census1 json file

--- cit-api/entrypoint.pipeline.sh
+++ cit-api/entrypoint.pipeline.sh
@@ -2,8 +2,8 @@
 
 ./wait-for-it.sh ${POSTGRES_HOST}:5432
 
-mkdir -p data/import/bucket_1
-
+mkdir -p data/import/bucket1
+cp 1census.json data/import/bucket1
 python3 manage.py migrate
 python3 manage.py bucket_1
 python3 manage.py runserver 0.0.0.0:8000
