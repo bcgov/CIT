2c4957c558b5b1936be9b464c55998a243c84dba
Kiranmai Arva <136566@NTTDATA.COM>
2022-03-05 11:56:59 -0600

    Added Housing Data-V1

--- cit-api/pipeline/importers/utils.py
+++ cit-api/pipeline/importers/utils.py
@@ -1,4 +1,5 @@
 import csv
+from numpy import int64
 import requests
 import copy
 import math
@@ -706,7 +707,6 @@ def calculate_muni_or_rd(instance):
 def import_housing(URL):
     #csv_file = 'total.csv'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
-    D = {'Jan' : '01','Feb' : '02','Mar' : '03','Apr' : '04','May' : '05','Jun' : '06','Jul' : '07','Aug' : '08','Sep' : '09','Oct' : '10','Nov' : '11','Dec' : '12'}
     url = "https://www2.gov.bc.ca/assets/gov/data/statistics/economy/building-permits/total.csv"
     s = requests.get(url,headers=headers)
     if s.ok:
@@ -716,11 +716,12 @@ def import_housing(URL):
             data.rename(columns={data.columns[0]:'census_subdivision_id'}, inplace=True)
             data.drop(data.columns[1], axis=1,inplace=True)
             data=data[(data['census_subdivision_id'].str.contains("[a-zA-Z]")==False) & (data['census_subdivision_id'].str.len() > 4)] 
-            data = data.melt(id_vars=['census_subdivision_id'], var_name='YearMonth', value_name='Total_Building_Permits ')
-            data['Month'] = data['YearMonth'].str.split('-').str[1]
-            data['year'] = data['YearMonth'].str.split('-').str[0]
+            data = data.melt(id_vars=['census_subdivision_id'], var_name='yearmonth', value_name='total_building_permits ')
+            data['census_subdivision_id'] = data['census_subdivision_id'].astype(str).astype(int64)
+            data['month'] = data['yearmonth'].str.split('-').str[1]
+            data['year'] = data['yearmonth'].str.split('-').str[0]
             data['year']=pd.to_datetime(data['year'], format = '%y',errors='coerce').dt.year
-            data['Month']=pd.to_datetime(data['Month'], format = '%b',errors='coerce').dt.month
+            data['month']=pd.to_datetime(data['month'], format = '%b',errors='coerce').dt.month
 
 
     
--- cit-api/pipeline/models/Housing_Data.py
+++ cit-api/pipeline/models/Housing_Data.py
@@ -4,16 +4,16 @@ from django.contrib.gis.db import models
 
 class Housing_Data(models.Model):
     census_subdivision_id	 = models.IntegerField(null=False, blank=False)
-    YearMonth = models.CharField(max_length=127,null=True)
+    yearmonth = models.CharField(max_length=127,null=True)
     year = models.IntegerField(null=True)
-    Month = models.IntegerField(null=True)
-    Total_Building_Permits = models.DecimalField(max_digits=16, decimal_places=4, null=True)
+    month = models.IntegerField(null=True)
+    total_building_permits = models.DecimalField(max_digits=16, decimal_places=4, null=True)
     
 
     class Meta:
         ordering = ("id", )
         managed = True
-        constraints = [models.UniqueConstraint(fields = ['census_subdivision_id','Month','year'],name = 'Primary Key Constraint')]
+        constraints = [models.UniqueConstraint(fields = ['census_subdivision_id','month','year'],name = 'Primary_Key_Constraint')]
 
     def __str__(self):
         return self.name
