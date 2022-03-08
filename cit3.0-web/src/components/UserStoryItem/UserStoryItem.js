b2fbd205f8d79ee28691b07e673a55159719c786
Arnel Balisi <arnel.balisi@gmail.com>
2022-03-08 10:21:30 -0500

    refactoring

--- cit3.0-web/src/components/UserStoryItem/UserStoryItem.js
+++ cit3.0-web/src/components/UserStoryItem/UserStoryItem.js
@@ -5,7 +5,9 @@ import ReactHtmlParser from "react-html-parser";
 import "./UserStoryItem.css";
 
 export default function UserStoryItem({ userStory, onUserStoryChange }) {
-  let selectStyle = {
+  const yesNo = userStory.user_story_paths.find((x) => x.label === "Yes");
+
+  const selectStyle = {
     container: (base) => ({
       ...base,
       flex: 0,
@@ -19,7 +21,7 @@ export default function UserStoryItem({ userStory, onUserStoryChange }) {
     }),
     menu: (base) => ({
       ...base,
-      width: 400,
+      width: yesNo ? 100 : 400,
       border: "solid white 2px",
     }),
     menuList: (base) => ({
@@ -39,7 +41,7 @@ export default function UserStoryItem({ userStory, onUserStoryChange }) {
       borderBottom: "2px solid #376fa3",
       borderRadius: "0",
       boxShadow: "none",
-      width: 330,
+      width: yesNo ? 100 : 330,
     }),
     indicatorSeparator: (base) => ({
       ...base,
@@ -54,59 +56,6 @@ export default function UserStoryItem({ userStory, onUserStoryChange }) {
       text: "",
     }),
   };
-
-  const yesNoStyle = {
-    container: (base) => ({
-      ...base,
-      flex: 0,
-      paddingRight: 8,
-    }),
-    option: (base, state) => ({
-      ...base,
-      border: "solid white 1px",
-      color: "#ffffff",
-      backgroundColor: state.isSelected ? "#003366" : "#3288D9",
-    }),
-    menu: (base) => ({
-      ...base,
-      width: 100,
-      border: "solid white 2px",
-    }),
-    menuList: (base) => ({
-      ...base,
-      border: "solid white 2px",
-    }),
-    singleValue: (base) => ({
-      ...base,
-      color: "#3288D9",
-      fontSize: "1.1em",
-    }),
-    control: (base) => ({
-      ...base,
-      borderTop: "none",
-      borderLeft: "none",
-      borderRight: "none",
-      borderBottom: "2px solid #376fa3",
-      borderRadius: "0",
-      boxShadow: "none",
-      width: 100,
-    }),
-    indicatorSeparator: (base) => ({
-      ...base,
-      display: "none",
-    }),
-    dropdownIndicator: (base) => ({
-      ...base,
-      color: "#3288d9",
-    }),
-    placeholder: (base) => ({
-      ...base,
-      text: "",
-    }),
-  };
-
-  const yesNo = userStory.user_story_paths.filter((x) => x.label === "Yes");
-  if (yesNo.length > 0) selectStyle = yesNoStyle;
 
   return (
     <>
