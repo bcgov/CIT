d60ead059b6b8550cf634587748c5fa5a660abdf
Liliana Tang <35161494+lilianatang@users.noreply.github.com>
2022-03-14 21:08:32 -0600

    added tourism region data source and imported data into bucket_@_semiannually

--- cit-api/pipeline/management/commands/bucket_2_semiannually.py
+++ cit-api/pipeline/management/commands/bucket_2_semiannually.py
@@ -41,7 +41,7 @@ class Command(BaseCommand):
         import_data_sources()
         #Ensure that the data sources are updated
         data_resources = DataSource.objects.filter(name__in=[
-            'Housing_Data', 'municipalities',  'regional_districts', 'school_districts', 'tsunami_zones', 'wildfires_zones', 'tourism_regions'
+            'tourism_regions', 'Housing_Data', 'municipalities',  'regional_districts', 'school_districts', 'tsunami_zones', 'wildfires_zones'
         ])
 
         for resource in data_resources:
