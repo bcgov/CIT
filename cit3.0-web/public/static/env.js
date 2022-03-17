6a2c08ca3f42fce03faa236150e07aff9d457276
Arnel Balisi <arnel.balisi@gmail.com>
2022-03-17 07:31:46 -0400

    change Powerbi report ID

--- cit3.0-web/public/static/env.js
+++ cit3.0-web/public/static/env.js
@@ -8,7 +8,7 @@
   window.env.snowplowUrl = "spt.apps.gov.bc.ca";
   window.env.geocoderKey = "";
   window.env.routePlannerKey = "";
-  window.env.pbiReportIdPublic = "c5ecf762-11d7-4aa6-ad06-05fcc8912561";
+  window.env.pbiReportIdPublic = "0d120da3-3d1a-4edb-9b0e-65f0890e3991";
   window.env.pbiReportIdInternal = "c5ecf762-11d7-4aa6-ad06-05fcc8912561";
   window.env.pbiReportIdCompare = "43ed4787-b50e-4e39-8863-bedcb074e13a";
   window.env.pbiReportIdSearch = "d1b121dc-2c0c-4df6-8de3-4ac66defe814";
