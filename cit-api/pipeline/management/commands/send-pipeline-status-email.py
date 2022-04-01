8a1432294bebc28ecc313f373880c5acc660c269
Jessica Stratton <jessicajstratton@outlook.com>
2022-04-01 15:30:45 -0700

    stringify

--- cit-api/pipeline/management/commands/send-pipeline-status-email.py
+++ cit-api/pipeline/management/commands/send-pipeline-status-email.py
@@ -55,9 +55,9 @@ def send_pipeline_status_email():
         print(response.text)
 
 def construct_email_body():
-    string= "The data import pipeline"+ os.environ.get("BUCKET_COMMAND")+ "has completed"+ os.environ.get("JOB_STATUS")
+    string= str("The data import pipeline"+ os.environ.get("BUCKET_COMMAND")+ "has completed"+ os.environ.get("JOB_STATUS"))
     return string
 
 def construct_email_subject():
-    string = "The scheduled job " + os.environ.get("BUCKET_COMMAND") +" was " + os.environ.get("JOB_STATUS")
+    string = str("The scheduled job " + os.environ.get("BUCKET_COMMAND") +" was " + os.environ.get("JOB_STATUS"))
     return string
