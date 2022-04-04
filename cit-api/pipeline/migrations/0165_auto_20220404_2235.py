d68a126edb3ba4557d0a0bf2daaa4745ec2ebf3e
Liliana Tang <35161494+lilianatang@users.noreply.github.com>
2022-04-04 16:54:37 -0600

    fixed migration conflicts again

--- cit-api/pipeline/migrations/0165_auto_20220404_2235.py
+++ cit-api/pipeline/migrations/0165_auto_20220404_2235.py
@@ -20,15 +20,5 @@ class Migration(migrations.Migration):
             model_name='phdemographicdistribution',
             name='census_subdivision',
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='pipeline.CEN_PROF_DETAILED_CSD_ATTRS_SP'),
-        ),
-        migrations.CreateModel(
-            name='BusinessesByCSD',
-            fields=[
-                ('naics_code', models.IntegerField(primary_key=True, serialize=False)),
-                ('employee_class', models.IntegerField()),
-                ('number_of_businesses', models.IntegerField()),
-                ('sector', models.CharField(max_length=127, null=True)),
-                ('census_subdivision', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='pipeline.CEN_PROF_DETAILED_CSD_ATTRS_SP')),
-            ],
-        ),
+        )
     ]
