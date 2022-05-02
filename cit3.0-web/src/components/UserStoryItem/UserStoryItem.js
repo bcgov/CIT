8707f22aa7c56eced1574eabc134381a31691d26
Arnel Balisi <arnel.balisi@gmail.com>
2022-05-02 14:15:28 -0400

    bc assessment with login

--- cit3.0-web/src/components/UserStoryItem/UserStoryItem.js
+++ cit3.0-web/src/components/UserStoryItem/UserStoryItem.js
@@ -14,6 +14,7 @@ export default function UserStoryItem({
     (x) => x.label === "Yes"
   );
   const userStoryNo = userStory.user_story_paths.find((x) => x.label === "No");
+
   const selectStyle = {
     container: (base) => ({
       ...base,
@@ -116,6 +117,9 @@ export default function UserStoryItem({
           !userStoryYes && (
             <Select
               placeholder=""
+              value={userStory.user_story_paths.find(
+                (x) => x.value === userStory.selectedValue
+              )}
               options={userStory.user_story_paths}
               styles={selectStyle}
               onChange={onUserStoryChange}
