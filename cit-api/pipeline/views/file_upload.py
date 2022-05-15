a9a5f08046b79a4d18f4412653d2ed500fac1f33
Liliana Tang <35161494+lilianatang@users.noreply.github.com>
2022-05-14 21:19:00 -0600

    handled bc assessment data right away after API upload

--- cit-api/pipeline/views/file_upload.py
+++ cit-api/pipeline/views/file_upload.py
@@ -4,6 +4,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from django.conf import settings
 from pipeline.serializers.file_upload import FileUpload
+from pipeline.models.bc_assessment.bc_assessment_census_subdivision import BCAssessmentCensusSubdivision
+from pipeline.models.bc_assessment.bc_assessment_economic_region import BCAssessmentEconomicRegion
+from pipeline.models.bc_assessment.bc_assessment_regional_district import BCAssessmentRegionalDistrict
+from pipeline.importers.utils import *
 
 UPLOAD_DIR = settings.MEDIA_ROOT
 
@@ -29,3 +33,18 @@ def handle_uploaded_file(f):
     with open(filepath, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    import_bc_assessment(filepath)
+
+def import_bc_assessment(filepath):
+    if 'bca_summary_by_cd_use.csv' in filepath:
+        data = pd.read_csv(filepath)
+        data.columns= data.columns.str.lower()
+        write_to_db(BCAssessmentRegionalDistrict, data)
+    elif 'bca_summary_by_csd_use.csv' in filepath:
+        data = pd.read_csv(filepath)
+        data.columns= data.columns.str.lower()
+        write_to_db(BCAssessmentCensusSubdivision, data)
+    elif 'bca_summary_by_er_use.csv' in filepath:
+        data = pd.read_csv(filepath)
+        data.columns= data.columns.str.lower()
+        write_to_db(BCAssessmentEconomicRegion, data)
