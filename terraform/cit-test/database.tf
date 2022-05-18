60526863294a3cbe3ed98a204315bc716ba23e2f
Chris <chris@bashbang.com>
2022-05-18 09:42:55 -0700

    change range for webapp

--- terraform/cit-test/database.tf
+++ terraform/cit-test/database.tf
@@ -32,8 +32,8 @@ resource "azurerm_postgresql_firewall_rule" "api-psql-webapp-firewall" {
   name                = "api-webapp"
   resource_group_name = var.azure_resource_group
   server_name         = azurerm_postgresql_server.postgres.name
-  start_ip_address    = "20.151.0.0"
-  end_ip_address      = "20.151.255.255"
+  start_ip_address    = "20.0.0.0"
+  end_ip_address      = "20.255.255.255"
 }
 
 resource "azurerm_postgresql_firewall_rule" "jessica-psql-firewall" {
