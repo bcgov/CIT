ffce55c346f615771b6d3391c1b2b194bdfcb0a8
Liliana Tang <35161494+lilianatang@users.noreply.github.com>
2022-05-21 15:36:19 -0600

    finished decoupling the data pipeline

--- cit-api/pipeline/importers/bucket2_regional_districts.py
+++ cit-api/pipeline/importers/bucket2_regional_districts.py
@@ -5,12 +5,14 @@ import urllib.request
 
 
 def import_data_sources():
-    DATA_SOURCES_FILENAME = 'data/import/bucket2/semiannually/2regional_districts.json'
+    DATA_SOURCES_FILENAME = "data/import/bucket2/semiannually/2regional_districts.json"
 
     with open(DATA_SOURCES_FILENAME) as f:
         data_sources = json.loads(f.read())
+        #print(data_sources)
 
     for data_source in data_sources:
+        #print(data_source)
         dataset, created = DataSource.objects.update_or_create(name=data_source.pop("name"), defaults={**data_source})
 
         print("dataset", dataset)
