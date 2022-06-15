852c2bf85802882eb6e027e13eb9b4c7bfeced90
Kiranmai Arva <136566@NTTDATA.COM>
2022-06-15 13:18:45 -0600

    Updated code for cit_regions_distribution view

--- cit-api/pipeline/importers/utils.py
+++ cit-api/pipeline/importers/utils.py
@@ -786,78 +786,118 @@ def remove_dependencies():
 def add_dependencies():
     with connection.cursor() as cursor:
         cursor.execute("""CREATE OR REPLACE VIEW public.cit_regions_distribution_vw AS
-              SELECT DISTINCT 'All of British Columbia'::text AS zone_type,
+               SELECT DISTINCT 'All of British Columbia'::text AS zone_type,
     'All of British Columbia'::text AS zone_name,
-	1::text AS zone_id,
-    csd.census_subdivision_id
+    1::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	'All of British Columbia'::text as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_cen_prof_detailed_csd_attrs_sp csd
 UNION
  SELECT DISTINCT 'Regional District'::text AS zone_type,
     rd.name AS zone_name,
-	rd.area_id::text AS zone_id,
-    csd.census_subdivision_id
+    rd.area_id::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	rd.name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_linkagewithcensus lk
      JOIN pipeline_cen_prof_detailed_csd_attrs_sp csd ON lk.census_subdivision_id = csd.census_subdivision_id
      JOIN pipeline_regionaldistrict rd ON lk.regional_district_id = rd.area_id::bigint::double precision
 UNION
  SELECT DISTINCT 'Census Subdivision'::text AS zone_type,
     csd.census_subdivision_name AS zone_name,
-	csd.census_subdivision_id::text AS zone_id,
-    csd.census_subdivision_id
+    csd.census_subdivision_id::text AS zone_id,
+    csd.census_subdivision_id,
+	csd.census_subdivision_type_desc as census_comm_type,
+	case when csd.census_subdivision_type_desc is null then csd.census_subdivision_name else concat(csd.census_subdivision_name,' (', csd.census_subdivision_type_desc, ')') end as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_cen_prof_detailed_csd_attrs_sp csd
+UNION
+ SELECT DISTINCT 'Community'::text AS zone_type,
+    com.place_name AS zone_name,
+    com.id::text AS zone_id,
+    com.census_subdivision_id,
+	com.community_type as census_comm_type,
+	com.place_name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
+   FROM pipeline_community com
+   left join pipeline_cen_prof_detailed_csd_attrs_sp csd on com.census_subdivision_id = csd.census_subdivision_id
 UNION
  SELECT 'Tourism Region'::text AS zone_type,
     tr.tourism_region_name AS zone_name,
-	tr.tourism_region_id::text AS zone_id,
-    csd.census_subdivision_id
+    tr.tourism_region_id::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	tr.tourism_region_name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_linkagewithcensus lk
      JOIN pipeline_cen_prof_detailed_csd_attrs_sp csd ON lk.census_subdivision_id = csd.census_subdivision_id
      JOIN pipeline_tourismregion tr ON lk.tourism_region_id = tr.tourism_region_id::text
 UNION
  SELECT 'Economic Region'::text AS zone_type,
     er.name AS zone_name,
-	er.economic_region_id::text AS zone_id,
-    csd.census_subdivision_id
+    er.economic_region_id::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	er.name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_linkagewithcensus lk
      JOIN pipeline_cen_prof_detailed_csd_attrs_sp csd ON lk.census_subdivision_id = csd.census_subdivision_id
      JOIN pipeline_censuseconomicregion er ON lk.economic_region_id = er.economic_region_id
 UNION
  SELECT 'Wildfire Zone'::text AS zone_type,
     wf.zone_name,
-	wf.zone_id::text,
-    csd.census_subdivision_id
+    wf.zone_id::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	wf.zone_name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_linkagewithcensus lk
      JOIN pipeline_cen_prof_detailed_csd_attrs_sp csd ON lk.census_subdivision_id = csd.census_subdivision_id
      JOIN pipeline_bcwildfirezone wf ON lk.bc_fire_zone_id = wf.zone_id
 UNION
  SELECT 'Tsunami Zone'::text AS zone_type,
     tz.tsunami_zone_name AS zone_name,
-	tz.name::text AS zone_id,
-    csd.census_subdivision_id
+    tz.name::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	tz.tsunami_zone_name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_linkagewithcensus lk
      JOIN pipeline_cen_prof_detailed_csd_attrs_sp csd ON lk.census_subdivision_id = csd.census_subdivision_id
      JOIN pipeline_tsunamizone tz ON lk.tsunami_notification_zone_id = tz.name::bigint::double precision
 UNION
  SELECT 'Health Authority'::text AS zone_type,
     ha.name AS zone_name,
-	ha.id::text AS zone_id,
-    csd.census_subdivision_id
+    ha.id::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	ha.name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_linkagewithcensus lk
      JOIN pipeline_cen_prof_detailed_csd_attrs_sp csd ON lk.census_subdivision_id = csd.census_subdivision_id
      JOIN pipeline_healthauthorityboundary ha ON lk.health_authority_id = ha.id
 UNION
  SELECT 'School District'::text AS zone_type,
     sd.name AS zone_name,
-	sd.area_id::text AS zone_id,
-    csd.census_subdivision_id
+    sd.area_id::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	sd.name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_linkagewithcensus lk
      JOIN pipeline_cen_prof_detailed_csd_attrs_sp csd ON lk.census_subdivision_id = csd.census_subdivision_id
      JOIN pipeline_schooldistrict sd ON lk.school_district_id = sd.area_id
 UNION
  SELECT 'Natural Resource Region'::text AS zone_type,
     nrr.name AS zone_name,
-	nrr.id::text AS zone_id,
-    csd.census_subdivision_id
+    nrr.id::text AS zone_id,
+    csd.census_subdivision_id,
+	null as census_comm_type,
+	nrr.name as zone_name_census_type,
+	csd.census_subdivision_name as zone_name_census_name
    FROM pipeline_linkagewithcensus lk
      JOIN pipeline_cen_prof_detailed_csd_attrs_sp csd ON lk.census_subdivision_id = csd.census_subdivision_id
      JOIN pipeline_naturalresourceregion nrr ON lk.natural_resource_region_id = nrr.id;""")
