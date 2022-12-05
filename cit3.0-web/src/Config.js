73d53a7d0783e91a0a9d0c4232789b009df466bb
Chris <chris@bashbang.com>
2022-12-05 14:42:13 -0800

    pkceMethod added to config.js

--- cit3.0-web/src/Config.js
+++ cit3.0-web/src/Config.js
@@ -4,6 +4,7 @@ const Config = {
     url: window.env.keycloakUrl,
     realm: window.env.keycloakRealm,
     clientId: window.env.keycloakClient,
+    pkceMethod: "S256",
   },
   snowplowUrl: window.env.snowplowUrl,
   geocoderKey: window.env.geocoderKey,
