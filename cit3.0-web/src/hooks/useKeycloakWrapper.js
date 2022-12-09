e214bf1c91d9e905aeb8b95f605cc4d22cf803d3
Jessica Stratton <jessicajstratton@outlook.com>
2022-12-09 05:41:20 -0800

    syntax

--- cit3.0-web/src/hooks/useKeycloakWrapper.js
+++ cit3.0-web/src/hooks/useKeycloakWrapper.js
@@ -40,11 +40,11 @@ export function useKeycloakWrapper() {
   /**
    * Return the user's identity provider
    */
-   const idp = () =>
-     userInfo &&
-     userInfo.preferred_username.slice(
-         userInfo.preferred_username.indexOf("@") + 1
-     );
+  const idp = () =>
+    userInfo &&
+    userInfo.preferred_username.slice(
+        userInfo.preferred_username.indexOf("@") + 1
+    );
 
   /**
    * Return the user's first name
