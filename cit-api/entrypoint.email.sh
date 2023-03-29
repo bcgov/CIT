401cc7b246489c429e442ca0de58071c2c9d187b
Jessica Stratton <jessicajstratton@outlook.com>
2023-03-29 13:43:33 -0700

    troubleshooting

--- cit-api/entrypoint.email.sh
+++ cit-api/entrypoint.email.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+ls -lstra 
 
-./cit-api/wait-for-it.sh ${POSTGRES_HOST}:5432
+cd cit-api 
 
-python3 ./cit-api/manage.py ${EMAIL_TYPE}
+wait-for-it.sh ${POSTGRES_HOST}:5432
+
+python3 manage.py ${EMAIL_TYPE}
 
 
