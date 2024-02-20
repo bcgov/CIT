5c36b263511d4b884608927e18e39f392a3bbca7
Jason Syrotuck <jasyrotuck@gmail.com>
2024-02-20 13:31:34 -0800

    remove other migrations

--- cit-api/pipeline/migrations/0192_rename_csd_centroid_to_match_old_xl_file.py
+++ cit-api/pipeline/migrations/0192_rename_csd_centroid_to_match_old_xl_file.py
@@ -6,65 +6,12 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pipeline', '0191_municipaltaxrates_alter_csdcentroid_options_and_more'),
+        ("pipeline", "0191_municipaltaxrates_alter_csdcentroid_options_and_more"),
     ]
 
     operations = [
-        migrations.DeleteModel(
-            name='CSDCoreHousingNeed',
-        ),
-        migrations.AlterField(
-            model_name='community',
-            name='has_any_k12_school',
-            field=models.BooleanField(null=True),
-        ),
-        migrations.AlterField(
-            model_name='community',
-            name='incorporated',
-            field=models.BooleanField(null=True),
-        ),
-        migrations.AlterField(
-            model_name='community',
-            name='is_coastal',
-            field=models.BooleanField(null=True),
-        ),
-        migrations.AlterField(
-            model_name='nbdphhspeeds',
-            name='phh_id',
-            field=models.IntegerField(primary_key=True, serialize=False),
-        ),
-        migrations.AlterField(
-            model_name='opportunity',
-            name='nearest_first_nations',
-            field=models.ManyToManyField(blank=True, db_column='nearest_first_nations', to='pipeline.indianreservebanddistance'),
-        ),
-        migrations.AlterField(
-            model_name='opportunity',
-            name='nearest_municipalities',
-            field=models.ManyToManyField(blank=True, db_column='nearest_municipalities', to='pipeline.municipalitydistance'),
-        ),
-        migrations.AlterField(
-            model_name='opportunity',
-            name='opportunity_preferred_development',
-            field=models.ManyToManyField(db_column='opportunity_preferred_development', to='pipeline.preferreddevelopment'),
-        ),
-        migrations.AlterField(
-            model_name='phdemographicdistribution',
-            name='phh_id',
-            field=models.IntegerField(primary_key=True, serialize=False),
-        ),
-        migrations.AlterField(
-            model_name='project',
-            name='is_earliest_entry',
-            field=models.BooleanField(null=True),
-        ),
-        migrations.AlterField(
-            model_name='project',
-            name='is_latest_entry',
-            field=models.BooleanField(null=True),
-        ),
         migrations.AlterModelTable(
-            name='csdcentroid',
-            table='c2021_FeatureToPoint_TableToExcel',
+            name="csdcentroid",
+            table="c2021_FeatureToPoint_TableToExcel",
         ),
     ]
