b30aac2ca0f0f95d50a679c8c855731b6d981568
Jason Syrotuck <jasyrotuck@gmail.com>
2024-03-04 16:00:59 -0800

    fix to restore original order

--- cit-api/pipeline/migrations/0193_delete_bad_csd_centroid_table.py
+++ cit-api/pipeline/migrations/0193_delete_bad_csd_centroid_table.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pipeline', '0193_csdsmallbusinesses_and_more'),
+        ('pipeline', '0192_rename_csd_centroid_to_match_old_xl_file'),
     ]
 
     operations = [
