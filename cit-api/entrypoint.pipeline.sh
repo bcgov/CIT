a18a29fb20282613a0823c31f8c85b2e477102f2
Jason Syrotuck <jasyrotuck@gmail.com>
2024-03-13 15:26:41 -0700

    try this

--- cit-api/entrypoint.pipeline.sh
+++ cit-api/entrypoint.pipeline.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+echo docker-compose --version
 
 ./wait-for-it.sh ${POSTGRES_HOST}:5432
 
 python3 manage.py migrate
 python3 manage.py ${BUCKET_COMMAND}
 
-exit $?
+if [ $? -eq 0 ]; then
+    echo "Bucket command executed successfully"
+else
+    echo "Bucket command failed"
+    exit 21
+fi
