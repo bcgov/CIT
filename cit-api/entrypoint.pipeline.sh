a6b4a958f6fa68c9893139945aad2c299e791d92
Jason Syrotuck <jasyrotuck@gmail.com>
2024-03-13 16:39:11 -0700

    simplify return value check

--- cit-api/entrypoint.pipeline.sh
+++ cit-api/entrypoint.pipeline.sh
@@ -4,10 +4,4 @@
 python3 manage.py migrate
 python3 manage.py ${BUCKET_COMMAND}
 
-if [ $? -eq 0 ]; then
-    echo "Bucket command executed successfully"
-else
-    echo "Bucket command failed"
-    exit 21
-fi
-exit 3
+exit $?
