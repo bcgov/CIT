03f4010837a114fe86c84fe6eea51edacf856f7d
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-06-19 11:50:25 -0700

    chore: make banner clickable, update image

--- cit3.0-web/src/components/Page/UserStory/UserStory.js
+++ cit3.0-web/src/components/Page/UserStory/UserStory.js
@@ -356,10 +356,12 @@ export default function UserStory() {
 
   const header = (
     <>
-      <img
-        src="/images/CIT_CIOT_banner.png"
-        alt="The Community Information Tool and the Community Investment Opportunities Tool will be decommissioned on July 3, 2024. If you have any questions, please contact connectedcommunities@gov.bc.ca"
-      />
+      <a href="mailto:connectedcommunities@gov.bc.ca">
+        <img
+          src="/images/CIT_CIOT_banner.png"
+          alt="The Community Information Tool and the Community Investment Opportunities Tool will be decommissioned on July 3, 2024. If you have any questions, please contact connectedcommunities@gov.bc.ca"
+        />
+      </a>
       <h1>Welcome to the Community Information Tool.</h1>
       <p className="mb-3">
         The Community Information Tool compiles over 40 data sets offering
