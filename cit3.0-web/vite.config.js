eb992e540218062b3650fed7e0c95f4ca3d98b69
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-02-24 10:27:03 -0800

    chore: cleanup

--- cit3.0-web/vite.config.js
+++ cit3.0-web/vite.config.js
@@ -2,43 +2,10 @@ import { defineConfig, transformWithEsbuild } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // {
-    //   name: "load+transform-js-files-as-jsx",
-    //   async transform(code, id) {
-    //     if (!id.match(/.*\.js$/)) {
-    //       return null;
-    //     }
-
-    //     // Use the exposed transform from vite, instead of directly
-    //     // transforming with esbuild
-    //     return transformWithEsbuild(code, id, {
-    //       loader: "jsx",
-    //       jsx: "automatic", // 👈 this is important
-    //     });
-    //   },
-    // },
-  ],
+  plugins: [react()],
   server: {
-    port: 3000, // Change this if needed
+    port: 3000,
   },
-  //   esbuild: {
-  //     loader: {
-  //       ".js": "jsx", // Add this line to handle .js files as JSX
-  //     },
-  //     include: [/\.js$/],
-  //   },
-  //   optimizeDeps: {
-  //     esbuildOptions: {
-  //       loader: {
-  //         ".js": "jsx",
-  //       },
-  //     },
-  //   },
-  //   optimizeDeps: {
-  //     include: ["shared-components"],
-  //   },
   resolve: {
     alias: {
       "shared-components/build/components/loader/Loader.css":
