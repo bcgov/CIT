6de4f7a9a382c46db2e78233db60f2d2978b8682
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-04-10 13:15:18 -0700

    chore: various pipeline fixes

--- cit-api/pipeline/importers/data_sources.py
+++ cit-api/pipeline/importers/data_sources.py
@@ -1,7 +1,7 @@
 from pipeline.importers.base_importer import BaseImporter
 
 class AllDataSourcesImporter(BaseImporter):
-    DATA_SOURCES = ["data/data_sources.json"]
+    DATA_SOURCES = ["data/import/datafiles/data_sources.json"]
 
 def import_data_sources():
     AllDataSourcesImporter.import_data_sources()
